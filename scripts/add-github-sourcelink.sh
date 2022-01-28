842a19a4b7f8fc3866d37725b75af5363bc190d8
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-28 21:45:41 +0100

    Add package_name parameter to script.

--- scripts/add-github-sourcelink.sh
+++ scripts/add-github-sourcelink.sh
@@ -3,12 +3,14 @@
 ###################################################################################################
 # add-github-sourcelink                                                                           #
 ###################################################################################################
-# Adds the Microsoft.SourceLink.GitHub package to projects in the solution or the project file.   #
-# Use it in the git repository's root directory.                                                  #
+# Adds a NuGet package to projects in the solution or the project file. Use it in the git         #
+# repository's root directory.                                                                    #
 #                                                                                                 #
 # Use shellcheck (https://github.com/koalaman/shellcheck) for linting and shell-format            #
 # (https://marketplace.visualstudio.com/items?itemName=foxundermoon.shell-format) for styling.    #
 ###################################################################################################
+
+package_name="$1"
 
 function err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
@@ -42,7 +44,7 @@ function alter-project() {
     project_file="$1"
     [ -f "$project_file" ] || panic 4 "Couldn't find the project '$project_file' in '$PWD'."
 
-    dotnet add "$project_file" package Microsoft.SourceLink.GitHub
+    dotnet add "$project_file" package "$package_name"
 }
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
@@ -50,12 +52,13 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     basename "$0"
     echo
     echo "USAGE"
-    echo -e "\tbash $(basename "$0") [solution.sln]\n"
+    echo -e "\tbash $(basename "$0") package_name [solution.sln]\n"
+    echo "package_name - The name of a NuGet package. The same you'd pass to 'dotnet add package'."
     echo "solution.sln - Optional argument to provide the path to the solution file. If none are"
     echo "               provided then the script looks for .sln or if none found then for .??proj"
     echo "               (i.e. csproj, fsproj, vbproj) in the current working directory."
-elif [ -f "$1" ]; then
-    alter-solution "$1"
+elif [ -f "$2" ]; then
+    alter-solution "$2"
 elif solutions=(./*.sln) && ((${#solutions[@]})) && [ -f "${solutions[0]}" ]; then
     for solution in "${solutions[@]}"; do
         alter-solution "$solution"
