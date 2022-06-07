1840659c602f77e5420cfb60ee4732606e7c8543
József Horváth <info@ministro.hu>
2022-06-07 21:00:31 +0200

    fixing interpolation

--- scripts/manifest-version.ps1
+++ scripts/manifest-version.ps1
@@ -14,16 +14,16 @@
             $Version
         )
 
-        (Get-Content -Raw $Manifest)`
-            -replace '(?<head>\[assembly:\s*Module\(([^\]]*Version\W*=\W*"))([^"]*)', ('${head}' + $Version)` |
+        (Get-Content -Raw $Manifest) -replace 
+            '(?<head>\[assembly:\s*Module\(([^\]]*Version\W*=\W*"))([^"]*)', "`${head}$Version" |
             Out-File $Manifest
 
          Write-Output "Version updated in $Manifest to $Version"
     }
 
     $manifests = Get-ChildItem $WorkDir -File -Recurse -Filter "Manifest.cs" |
-                   Select-String -List -Pattern 'OrchardCore.Modules.Manifest' | 
-                     Select-Object -ExpandProperty Path
+        Select-String -List -Pattern 'OrchardCore.Modules.Manifest' | 
+        Select-Object -ExpandProperty Path
 
     foreach ($manifest in $manifests) {
         Update-Manifest-Version $manifest $PackageVersion
