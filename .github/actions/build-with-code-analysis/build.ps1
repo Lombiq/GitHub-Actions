d0d842d1bec309c1b55a95196481509e14fc177d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-14 22:34:24 +0200

    Attempting to put build script into its own file

---
+++ .github/actions/build-with-code-analysis/build.ps1
@@ -0,0 +1,19 @@
+﻿npm install pnpm -g
+
+if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj)
+{
+    dotnet build src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj --configuration Release --verbosity ${{ inputs.build-verbosity }}
+}
+
+$buildSwitches = @(
+    '--configuration',
+    'Release',
+    '-warnaserror',
+    '-p:TreatWarningsAsErrors=true',
+    '-p:RunAnalyzersDuringBuild=true',
+    '-nologo',
+    '-consoleLoggerParameters:NoSummary',
+    '-verbosity:${{ inputs.build-verbosity }}'
+)
+
+dotnet build (Get-ChildItem *.sln).FullName @buildSwitches
