2c4368055f8fee2d089538bbd5939ab5c1c78819
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-15 01:52:19 +0200

    Moving test execution and related steps to separate action too

---
+++ .github/actions/run-tests/merge-failure-dumps.ps1
@@ -0,0 +1,7 @@
+﻿param ($directory)
+
+New-Item -Type Directory "$directory/FailureDumps"
+Get-ChildItem "$directory/test" -Recurse |
+    ? { $_.Name -eq 'FailureDumps' } |
+    % { $_.GetDirectories() } |
+    % { Move-Item $_.FullName "$directory/FailureDumps/${_.Name}" }
