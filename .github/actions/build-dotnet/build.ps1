9e89947851feb49a6c680b2492cf7480b0e5f245
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-15 12:46:11 +0200

    Strings as proper strings in PS

--- .github/actions/build-dotnet/build.ps1
+++ .github/actions/build-dotnet/build.ps1
@@ -8,14 +8,14 @@ if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj
 }
 
 $buildSwitches = @(
-    '--configuration',
-    'Release',
-    '-warnaserror',
-    '-p:TreatWarningsAsErrors=true',
+    "--configuration",
+    "Release",
+    "-warnaserror",
+    "-p:TreatWarningsAsErrors=true",
     "-p:RunAnalyzersDuringBuild=$EnableCodeAnalysis",
-    '-nologo',
-    '-consoleLoggerParameters:NoSummary',
-    '--verbosity' + $Verbosity
+    "-nologo",
+    "-consoleLoggerParameters:NoSummary",
+    "-verbosity:$Verbosity"
 )
 
 dotnet build (Get-ChildItem *.sln).FullName @buildSwitches
