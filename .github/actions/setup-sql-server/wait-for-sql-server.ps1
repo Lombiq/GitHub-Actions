efcf6c771c41f9648fa496a615f7d6e072df0a2d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-16 17:48:34 +0200

    Factoring out scripts from setup-sql-server too

---
+++ .github/actions/setup-sql-server/wait-for-sql-server.ps1
@@ -0,0 +1,30 @@
+﻿$maxTryCount = 10
+
+for ($i = 1; $i -le $maxTryCount; $i++)
+{
+    echo "Waiting for SQL Server to start. Attempt $i/$maxTryCount."
+
+    if ($Env:RUNNER_OS -eq "Windows")
+    {
+        sqlcmd -b -S .\SQLEXPRESS -Q "SELECT @@SERVERNAME as ServerName" 2>&1>$null
+    }
+    else
+    {
+        sqlcmd -b -U sa -P 'Password1!' -Q "SELECT @@SERVERNAME as ServerName" 2>&1>$null
+    }
+
+    if ($?)
+    {
+        echo "SQL Server is successfully started."
+        Exit 0
+    }
+
+    if ($i -eq $maxTryCount)
+    {
+        echo "SQL Server couldn't be started."
+        Exit 1
+    }
+
+    echo "SQL Server is not ready. Waiting 1 second."
+    Start-Sleep -s 1
+}
