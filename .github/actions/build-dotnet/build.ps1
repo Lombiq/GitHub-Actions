3f3eb7d3f6fe72b42fde51d9ad1122072c8ba883
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-16 17:05:11 +0200

    Fixing Release switch

--- .github/actions/build-dotnet/build.ps1
+++ .github/actions/build-dotnet/build.ps1
@@ -14,7 +14,7 @@ if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj
 Write-Output "Building solution."
 
 $buildSwitches = @(
-    "--configuration Release",
+    "--configuration:Release",
     "-warnaserror",
     "-p:TreatWarningsAsErrors=true",
     "-p:RunAnalyzersDuringBuild=$EnableCodeAnalysis",
