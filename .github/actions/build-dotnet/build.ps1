b3f0e4bb157aedd19741737ec206031ac82ce88c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-18 23:42:12 +0200

    Typo
    Co-authored-by: Dávid El-Saig <david.el-saig@lombiq.com>

--- .github/actions/build-dotnet/build.ps1
+++ .github/actions/build-dotnet/build.ps1
@@ -1,6 +1,6 @@
 ﻿param ($Verbosity, $EnableCodeAnalysis, $Version)
 
-# Notes on build switches that aren't self-explanatory':
+# Notes on build switches that aren't self-explanatory:
 # - /p:Retries and /p:RetryDelayMilliseconds are to retry builds if due to random locks it fails the first time.
 # - -warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
 #   errors." from breaking the build (since we treat warnings as errors).
