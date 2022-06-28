f236ff29dc72d36e1f3d94044c65644170169aa9
Dávid El-Saig <david.el-saig@lombiq.com>
2022-06-28 22:59:53 +0200

    Refactor build switch building.

--- .github/actions/build-dotnet/build.ps1
+++ .github/actions/build-dotnet/build.ps1
@@ -4,33 +4,31 @@
     [string] $Version,
     [string] $Switches = "")
 
+Write-Output ".NET version number: $Version"
+
 # Notes on build switches that aren't self-explanatory:
 # - -p:Retries and -p:RetryDelayMilliseconds are to retry builds if it fails the first time due to random locks.
 # - --warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
 #   errors." from breaking the build (since we treat warnings as errors).
 
-$buildSwitches = @(
-    "--configuration:Release",
-    "--nologo",
-    "--verbosity:$Verbosity",
-    "--warnaserror",
-    "--warnAsMessage:MSB3026",
-    "--consoleLoggerParameters:NoSummary",
-    "-p:TreatWarningsAsErrors=true",
-    "-p:RunAnalyzersDuringBuild=$EnableCodeAnalysis",
-    "-p:Retries=4",
-    "-p:RetryDelayMilliseconds=1000",
-    "-p:Version=$Version"
-)
+$defaultBuildSwitches = @"
+    --configuration:Release
+    --nologo
+    --verbosity:$Verbosity
+    --warnaserror
+    --warnAsMessage:MSB3026
+    --consoleLoggerParameters:NoSummary
+    -p:TreatWarningsAsErrors=true
+    -p:RunAnalyzersDuringBuild=$EnableCodeAnalysis
+    -p:Retries=4
+    -p:RetryDelayMilliseconds=1000
+    -p:Version=$Version
+    $Switches
+"@
 
-$switchEntries = ($Switches -split "`n") |
+$buildSwitches = $defaultBuildSwitches.Split("`n") |
     % { $_.Trim() } |
     ? { -not [string]::IsNullOrEmpty($_) }
-$switchEntries |
-    ? { $_ -notin $buildSwitches }
-    % { $buildSwitches += ,$_ }
-
-Write-Output ".NET version number: $Version"
 
 if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj)
 {
