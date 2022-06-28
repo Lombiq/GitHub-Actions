978bcafb0fb34e968e6d4c8db0259b0da5040c79
József Horváth <jozsef.horvath@lombiq.com>
2022-06-28 20:57:07 +0200

    adding edge to gh path

--- .github/actions/setup-edge-linux/setup-edge.ps1
+++ .github/actions/setup-edge-linux/setup-edge.ps1
@@ -6,6 +6,8 @@ function Program {
 
     bash -c "curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${EdgeVersion}_amd64.deb"
     bash -c "sudo apt install ./microsoft-edge-stable_${EdgeVersion}_amd64.deb"
+
+    echo "/opt/microsoft/msedge" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 }
 
 Program $args[0]
