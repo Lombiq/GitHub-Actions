f1c0b89ff6096b735b50cf612b142b9be859cb90
Dávid El-Saig <david.el-saig@lombiq.com>
2022-06-28 13:52:30 +0200

    More consistent `$buildSwitches`.

--- .github/actions/build-dotnet/build.ps1
+++ .github/actions/build-dotnet/build.ps1
@@ -1,22 +1,22 @@
 ﻿param ($Verbosity, $EnableCodeAnalysis, $Version)
 
 # Notes on build switches that aren't self-explanatory:
-# - /p:Retries and /p:RetryDelayMilliseconds are to retry builds if it fails the first time due to random locks.
-# - -warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
+# - -p:Retries and -p:RetryDelayMilliseconds are to retry builds if it fails the first time due to random locks.
+# - --warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
 #   errors." from breaking the build (since we treat warnings as errors).
 
 $buildSwitches = @(
     "--configuration:Release",
-    "-warnaserror",
+    "--nologo",
+    "--verbosity:$Verbosity",
+    "--warnaserror",
+    "--warnAsMessage:MSB3026",
+    "--consoleLoggerParameters:NoSummary",
     "-p:TreatWarningsAsErrors=true",
     "-p:RunAnalyzersDuringBuild=$EnableCodeAnalysis",
-    "-nologo",
-    "-consoleLoggerParameters:NoSummary",
-    "-verbosity:$Verbosity",
-    "/p:Retries=4",
-    "/p:RetryDelayMilliseconds=1000",
-    "-warnAsMessage:MSB3026",
-    "/p:Version=$Version"
+    "-p:Retries=4",
+    "-p:RetryDelayMilliseconds=1000",
+    "-p:Version=$Version"
 )
 
 Write-Output ".NET version number: $Version"
