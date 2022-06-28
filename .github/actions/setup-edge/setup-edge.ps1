74c0246c899b94b56c460e921994a634ee97268a
József Horváth <jozsef.horvath@lombiq.com>
2022-06-28 17:53:37 +0200

    adding setup-edge action

---
+++ .github/actions/setup-edge/setup-edge.ps1
@@ -0,0 +1,20 @@
+function Program {
+    param(
+        [string]
+        $EdgeLinuxVersion,
+        [string]
+        $EdgeWindowsVersion,
+    )
+
+    if ($Env:RUNNER_OS -eq "Windows")
+    {
+        choco install microsoft-edge --version $EdgeWindowsVersion -y
+    }
+    else
+    {
+        bash -c "curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_$EdgeLinuxVersion_amd64.deb"
+        bash -c "sudo apt install ./microsoft-edge-stable_$EdgeLinuxVersion_amd64.deb"
+    }
+}
+
+Program $args[0] $args[1]
