90b89d8ecce80dba21414780753cfa18ad3e7081
József Horváth <jozsef.horvath@lombiq.com>
2022-06-28 19:33:03 +0200

    test without install

--- .github/actions/setup-edge/setup-edge.ps1
+++ .github/actions/setup-edge/setup-edge.ps1
@@ -1,3 +1,27 @@
+function Get-ProcessOutput
+{
+    Param (
+        [Parameter(Mandatory=$true)]$FileName,
+        $Args
+    )
+    
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo.UseShellExecute = $false
+    $process.StartInfo.RedirectStandardOutput = $true
+    $process.StartInfo.RedirectStandardError = $true
+    $process.StartInfo.FileName = $FileName
+    if($Args) { $process.StartInfo.Arguments = $Args }
+    $out = $process.Start()
+    
+    $StandardError = $process.StandardError.ReadToEnd()
+    $StandardOutput = $process.StandardOutput.ReadToEnd()
+    
+    $output = New-Object PSObject
+    $output | Add-Member -type NoteProperty -name StandardOutput -Value $StandardOutput
+    $output | Add-Member -type NoteProperty -name StandardError -Value $StandardError
+    return $output
+}
+
 function Program {
     param(
         [string]
@@ -8,16 +32,7 @@ function Program {
 
     if ($Env:RUNNER_OS -eq "Windows")
     {
-        $installedVersion = (Get-ItemProperty -Path HKCU:\Software\Microsoft\Edge\BLBeacon -Name version).version
         
-        if ([string]::IsNullOrEmpty($installedVersion))
-        {
-            #choco install microsoft-edge --version $EdgeWindowsVersion -y
-        }
-        else
-        {
-            Write-Output "Installed edge version: ${installedVersion}"
-        }
     }
     else
     {
