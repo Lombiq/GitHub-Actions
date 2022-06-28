316ff481f117660a9d57426fa9eb38419a943ac3
József Horváth <jozsef.horvath@lombiq.com>
2022-06-28 20:35:59 +0200

    fixing variable name

--- .github/actions/setup-edge-linux/setup-edge.ps1
+++ .github/actions/setup-edge-linux/setup-edge.ps1
@@ -4,8 +4,8 @@ function Program {
         $EdgeVersion
     )
 
-    bash -c "curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${EdgeLinuxVersion}_amd64.deb"
-    bash -c "sudo apt install ./microsoft-edge-stable_${EdgeLinuxVersion}_amd64.deb"
+    bash -c "curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${EdgeVersion}_amd64.deb"
+    bash -c "sudo apt install ./microsoft-edge-stable_${EdgeVersion}_amd64.deb"
 }
 
 Program $args[0]
