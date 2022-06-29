3f754397fe3aeb17843045b5599c0702be5f39d9
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-29 21:43:48 +0200

    Using non-aliased ForEach-Object

--- .github/actions/test-dotnet/replace-xunit-max-parallel-threads.ps1
+++ .github/actions/test-dotnet/replace-xunit-max-parallel-threads.ps1
@@ -2,8 +2,8 @@ param ($MaxParallelThreads)
 
 Write-Output "Replacing maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
 
-($configFiles = Get-ChildItem "*/**/xunit.runner.json" -Recurse) | ForEach {
-     (Get-Content $_) | ForEach  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} | Set-Content $_
+($configFiles = Get-ChildItem "*/**/xunit.runner.json" -Recurse) | ForEach-Object {
+     (Get-Content $_) | ForEach-Object  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} | Set-Content $_
 }
 
 Write-Output "Replaced $($configFiles.Count) occurrences."
