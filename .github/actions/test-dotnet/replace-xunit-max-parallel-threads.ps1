1eadf7d0a6d10cbc6d6d146e6badb88490f51e87
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-06-29 21:46:15 +0200

    Getting rid of long line

--- .github/actions/test-dotnet/replace-xunit-max-parallel-threads.ps1
+++ .github/actions/test-dotnet/replace-xunit-max-parallel-threads.ps1
@@ -3,7 +3,9 @@ param ($MaxParallelThreads)
 Write-Output "Replacing maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
 
 ($configFiles = Get-ChildItem "*/**/xunit.runner.json" -Recurse) | ForEach-Object {
-     (Get-Content $_) | ForEach-Object  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} | Set-Content $_
+     (Get-Content $_) |
+        ForEach-Object  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} |
+        Set-Content $_
 }
 
 Write-Output "Replaced $($configFiles.Count) occurrences."
