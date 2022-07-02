ea6ecac54af6bd035a904d8ddd2555955f8b14fd
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-02 16:23:43 +0200

    Revert "Some random code cleanup."
    This reverts commit a825d3a1357683303140bedd9adbff015a02bcb7.

--- .github/actions/publish-nuget/Update-ManifestVersion.ps1
+++ .github/actions/publish-nuget/Update-ManifestVersion.ps1
@@ -2,16 +2,28 @@
     [string]
     $WorkDir,
     [string]
-    $Version
+    $PackageVersion
 )
+
+function Update-Manifest-Version {
+    param(
+        [string]
+        $Manifest,
+        [string]
+        $Version
+    )
+
+    (Get-Content -Raw $Manifest) -replace
+        '(?<head>\[assembly:\s*(Module|Theme)\(([^\]]*Version\W*=\W*"))([^"]*)', "`${head}$Version" |
+        Out-File $Manifest
+
+        Write-Output "Version updated in $Manifest to $Version"
+}
 
 $manifests = Get-ChildItem $WorkDir -File -Recurse -Filter "Manifest.cs" |
     Select-String -List -Pattern '(OrchardCore.Modules.Manifest|OrchardCore.DisplayManagement.Manifest)' |
     Select-Object -ExpandProperty Path
 
 foreach ($manifest in $manifests) {
-    $regex = '(?<head>\[assembly:\s*(Module|Theme)\(([^\]]*Version\W*=\W*"))([^"]*)', "`${head}$Version"
-    (Get-Content -Raw $manifest) -replace $regex | Out-File $manifest
-
-    Write-Output "Version updated in $manifest to $Version"
+    Update-Manifest-Version $manifest $PackageVersion
 }
