d0bc1d67cdbc6e0e332f2b39b148713c964735eb
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-04 04:59:58 +0200

    Clean up ps1 in `setup-edge-linux`.

--- .github/actions/setup-edge-linux/Initialize-Edge.ps1
+++ .github/actions/setup-edge-linux/Initialize-Edge.ps1
@@ -5,7 +5,9 @@ param(
     $EdgeVersion
 )
 
-bash -c "curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${EdgeVersion}_amd64.deb"
-bash -c "sudo apt install ./microsoft-edge-stable_${EdgeVersion}_amd64.deb"
+bash -c @"
+curl -O https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${EdgeVersion}_amd64.deb
+sudo apt install microsoft-edge-stable_${EdgeVersion}_amd64.deb
+"@
 
 Write-Output "/opt/microsoft/msedge" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
