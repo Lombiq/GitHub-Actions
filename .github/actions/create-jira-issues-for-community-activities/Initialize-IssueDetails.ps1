c8cf57d0b510bbbd5ba6555ca0f279b74d6bbefb
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-14 22:51:00 +0200

    Generating descriptions based on passed secrets

--- .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
+++ .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
@@ -5,10 +5,12 @@ switch ($GitHub.event_name)
     "discussion"
     {
         $summary = "Respond to `"$($GitHub.event.discussion.title)`""
+        $description = $Env:DISCUSSION_JIRA_ISSUE_DESCRIPTION
     }
     "issues"
     {
         $summary = "$($GitHub.event.issue.title) in $($GitHub.repository)"
+        $description = $Env:ISSUE_JIRA_ISSUE_DESCRIPTION
 
         $i = 0
         while($i -lt $GitHub.labels.Length -and $type -eq $null)
@@ -30,6 +32,7 @@ switch ($GitHub.event_name)
     "pull_request"
     {
         $summary = "Review `"$($GitHub.event.pull_request.title)`""
+        $description = $Env:PULL_REQUEST_JIRA_ISSUE_DESCRIPTION
     }
 }
 
@@ -39,4 +42,5 @@ if ($type -eq $null)
 }
 
 Write-Output "::set-output name=summary::$summary"
+Write-Output "::set-output name=description::$description"
 Write-Output "::set-output name=type::$type"
