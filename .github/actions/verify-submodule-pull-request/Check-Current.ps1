e5c600cf34a3d13066cab747d4002486031d39ac
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-15 15:50:54 +0200

    Factoring out PR title check to separate script

--- .github/actions/verify-submodule-pull-request/Check-Current.ps1
+++ .github/actions/verify-submodule-pull-request/Check-Current.ps1
@@ -1,6 +1,5 @@
-# Note that this script is also used from create-jira-issues-for-community-activities.
 param([string] $Title)
 
-if ($Title -match '^\s*\w+-\d+\s*:') { exit 0 }
+if (Check-PullRequestTitle $Title) { exit 0 }
 Set-Failed ('The pull request title is not in the expected format. Please start with the issue code followed by a ' +
     'colon and the title, e.g. "PROJ-123: My PR Title".')
