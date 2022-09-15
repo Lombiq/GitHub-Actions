b0d62274a7a7132751636b7d864711b5637dfba3
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-15 13:54:40 +0200

    PS syntax

--- .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
+++ .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
@@ -6,13 +6,13 @@ switch ($GitHub.event_name)
     {
         $summary = "Respond to `"$($GitHub.event.discussion.title)`""
         $description = $Env:DISCUSSION_JIRA_ISSUE_DESCRIPTION
-        $link = GitHub.event.discussion.html_url
+        $link = $GitHub.event.discussion.html_url
     }
     "issues"
     {
         $summary = "$($GitHub.event.issue.title) in $($GitHub.repository)"
         $description = $Env:ISSUE_JIRA_ISSUE_DESCRIPTION
-        $link = GitHub.event.issue.html_url
+        $link = $GitHub.event.issue.html_url
 
         $i = 0
         while($i -lt $GitHub.event.issue.labels.Length -and $type -eq $null)
@@ -35,7 +35,7 @@ switch ($GitHub.event_name)
     {
         $summary = "Review `"$($GitHub.event.pull_request.title)`""
         $description = $Env:PULL_REQUEST_JIRA_ISSUE_DESCRIPTION
-        $link = GitHub.event.pull_request.html_url
+        $link = $GitHub.event.pull_request.html_url
     }
 }
 
