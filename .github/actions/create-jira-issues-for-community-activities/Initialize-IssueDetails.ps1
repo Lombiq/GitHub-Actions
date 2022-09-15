599eb5b1657662c062c9d1a88837b4b1f61a608d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-15 16:47:17 +0200

    Simplification

--- .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
+++ .github/actions/create-jira-issues-for-community-activities/Initialize-IssueDetails.ps1
@@ -45,7 +45,6 @@ if ($null -eq $type)
 }
 
 Write-Output "::set-output name=summary::$summary"
-$jsonDescription = $description | ConvertTo-Json
-Write-Output "::set-output name=jsonDescription::$jsonDescription"
+Write-Output "::set-output name=jsonDescription::$($description | ConvertTo-Json)"
 Write-Output "::set-output name=type::$type"
 Write-Output "::set-output name=link::$link"
