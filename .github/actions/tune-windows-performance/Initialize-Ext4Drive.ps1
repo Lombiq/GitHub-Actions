833e19ca2909f336392db6eaa6e124d4605c374e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-21 01:27:49 +0200

    More debug code

--- .github/actions/tune-windows-performance/Initialize-Ext4Drive.ps1
+++ .github/actions/tune-windows-performance/Initialize-Ext4Drive.ps1
@@ -1,4 +1,5 @@
 # Diskpart uses an interactive mode. We could use /s to feed a script to it but then we'd need to generate the file if
 # dynamic data is needed. Doing the below just won't work in PS, piping the stdout to diskpart only works from the CMD.
-$vhdxPath = Join-Path $Env:GITHUB_WORKSPACE Workspace.vhdx
+$vhdxPath = "D:\a\Open-Source-Orchard-Core-Extensions\Open-Source-Orchard-Core-Extensions\Workspace.vhdx"
 cmd.exe /c "(echo create vdisk file='$vhdxPath' maximum=10240 type=expandable) | diskpart"
+Write-Output "\\.\PhysicalDrive$((Mount-VHD -Path $vhdxPath -PassThru | Get-Disk).Number)"
