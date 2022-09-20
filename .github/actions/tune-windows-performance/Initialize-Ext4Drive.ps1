1b0cb5d9e0e4c14a618997435dd239ffc542fcce
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-21 01:19:44 +0200

    Debug code

--- .github/actions/tune-windows-performance/Initialize-Ext4Drive.ps1
+++ .github/actions/tune-windows-performance/Initialize-Ext4Drive.ps1
@@ -1,4 +1,5 @@
 # Diskpart uses an interactive mode. We could use /s to feed a script to it but then we'd need to generate the file if
 # dynamic data is needed. Doing the below just won't work in PS, piping the stdout to diskpart only works from the CMD.
 $vhdxPath = Join-Path $Env:GITHUB_WORKSPACE Workspace.vhdx
+Write-Output $vhdxPath
 cmd.exe /c "(echo create vdisk file=\"$vhdxPath\" maximum=10240 type=expandable) | diskpart"
