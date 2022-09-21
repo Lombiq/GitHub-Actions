e2314e26c2902b5c357525fbd56ec7a8336b4dc6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-21 19:03:55 +0200

    Actually we don't need to know the drive count

--- .github/actions/tune-windows-performance/Initialize-BtrfsDrive.ps1
+++ .github/actions/tune-windows-performance/Initialize-BtrfsDrive.ps1
@@ -10,40 +10,14 @@ choco install winbtrfs
 # Diskpart uses an interactive mode. We thus use /s to feed a script to it.
 # You get 14 GB of storage space on GitHub-hosted runners, so erring on the safe side with 13 GB max size, see:
 # https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
+
+# mkbtrfs needs a drive letter to format, but we can't mount the drive until it's formatted, and diskpart can't format
+# with btrfs... So, formatting with NTFS first.
+
 @"
 create vdisk file='$vhdxPath' maximum=13312 type=expandable
 select vdisk file='$vhdxPath'
 attach vdisk
-list disk
-"@ > DiskpartCommands.txt
-
-$output = & diskpart /s DiskpartCommands.txt
-
-Write-Output $output
-
-# For some reason, Split() won't work with the "DiskPart successfully attached the virtual disk file." string, just new
-# lines.
-$listDiskOutput = $output.Split([Environment]::NewLine)
-
-$lineIndex = 0
-foreach ($line in $listDiskOutput)
-{
-    if ($line.Contains("DiskPart successfully attached the virtual disk file."))
-    {
-        break
-    }
-
-    $lineIndex++
-}
-
-# The first 4 lines are empty and the command's output header.
-$numberOfDisks = $listDiskOutput.Length - $lineIndex - 4
-$diskIndex = $numberOfDisks - 1
-
-# mkbtrfs needs a drive letter to format, but we can't mount the drive until it's formatted, and diskpart can't format
-# with btrfs... So, formatting with NTFS first.
-@"
-select vdisk file='$vhdxPath'
 clean
 create partition primary
 format fs=ntfs
@@ -63,7 +37,6 @@ while ($i -lt 10 -and (Get-Volume | Where-Object {$_.FileSystemLabel -eq "BtrfsD
 {
     Start-Sleep -Seconds 1
 }
-
 
 $driveLetter = (Get-Volume -FileSystemLabel "BtrfsDrive").DriveLetter
 New-Item -Path Workspace -ItemType SymbolicLink -Value "$($driveLetter):\\"
