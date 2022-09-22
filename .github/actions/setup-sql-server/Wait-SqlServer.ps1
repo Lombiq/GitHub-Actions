832010293b97f23511a4970e207835e8d642802a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-22 22:59:03 +0200

    Attempting SQL Server Tools installation for BuildJet

--- .github/actions/setup-sql-server/Wait-SqlServer.ps1
+++ .github/actions/setup-sql-server/Wait-SqlServer.ps1
@@ -1,4 +1,17 @@
-﻿$maxTryCount = 10
+if ($Env:RUNNER_OS -ne "Windows")
+{
+    # Taken from: https://learn.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools?view=sql-server-ver16#ubuntu.
+    curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+    sudo apt-get update 
+    sudo apt-get install mssql-tools unixodbc-dev
+    bash -c "'export PATH=\"$PATH:/opt/mssql-tools/bin\"' >> ~/.bash_profile"
+    bash -c "'export PATH=\"$PATH:/opt/mssql-tools/bin\"' >> ~/.bashrc"
+    bash -c "source ~/.bashrc"
+}
+
+
+$maxTryCount = 10
 
 for ($i = 1; $i -le $maxTryCount; $i++)
 {
