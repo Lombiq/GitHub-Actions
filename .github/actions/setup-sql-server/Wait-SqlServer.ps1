9706adea792dcd589f45d36680c671f03db6c96c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-22 23:08:03 +0200

    PS syntax

--- .github/actions/setup-sql-server/Wait-SqlServer.ps1
+++ .github/actions/setup-sql-server/Wait-SqlServer.ps1
@@ -5,8 +5,8 @@ if ($Env:RUNNER_OS -ne "Windows")
     curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
     sudo apt-get update 
     sudo apt-get install mssql-tools unixodbc-dev
-    bash -c "'export PATH=\"$PATH:/opt/mssql-tools/bin\"' >> ~/.bash_profile"
-    bash -c "'export PATH=\"$PATH:/opt/mssql-tools/bin\"' >> ~/.bashrc"
+    bash -c '\'export PATH="$PATH:/opt/mssql-tools/bin"\' >> ~/.bash_profile'
+    bash -c '\'export PATH="$PATH:/opt/mssql-tools/bin"\' >> ~/.bashrc'
     bash -c "source ~/.bashrc"
 }
 
