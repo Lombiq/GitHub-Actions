3093aa692d9c323042ae94fae535d22f8d22674a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-22 22:30:47 +0200

    Code styling

--- .github/actions/setup-sql-server/Initialize-SqlServer.ps1
+++ .github/actions/setup-sql-server/Initialize-SqlServer.ps1
@@ -5,6 +5,10 @@ if ($Env:RUNNER_OS -eq "Windows")
 else
 {
     docker pull mcr.microsoft.com/mssql/server &&
-    docker run --name sql2019 --env 'ACCEPT_EULA=Y' --env 'SA_PASSWORD=Password1!' --publish 1433:1433 --detach 'mcr.microsoft.com/mssql/server:2019-latest' &&
+    docker run --name sql2019 `
+        --env 'ACCEPT_EULA=Y' `
+        --env 'SA_PASSWORD=Password1!' `
+        --publish 1433:1433 `
+        --detach 'mcr.microsoft.com/mssql/server:2019-latest' &&
     docker exec --user 0 sql2019 bash -c 'mkdir /data; chmod 777 /data -R; chown mssql:root /data'
 }
