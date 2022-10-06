272d586924c3fc5ca58e6aa8d52e14d56518f384
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-06 16:14:23 +0200

    PS syntax

--- .github/actions/setup-sql-server/Initialize-SqlServer.ps1
+++ .github/actions/setup-sql-server/Initialize-SqlServer.ps1
@@ -4,8 +4,6 @@ if ($Env:RUNNER_OS -eq "Windows")
 }
 else
 {
-    docker pull mcr.microsoft.com/mssql/server &&
-
     $dockerRunSwitches = @{
         name = sql2019
         env = 'ACCEPT_EULA=Y'
@@ -13,7 +11,8 @@ else
         publish = 1433:1433
         detach = 'mcr.microsoft.com/mssql/server:2019-latest'
     }
-    docker run @dockerRunSwitches &&
 
+    docker pull mcr.microsoft.com/mssql/server &&
+    docker run @dockerRunSwitches &&
     docker exec --user 0 sql2019 bash -c 'mkdir /data; chmod 777 /data --recursive; chown mssql:root /data'
 }
