b48b99ce6ffbe482ce7b4abf9675fd8bae365ac7
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-06 15:55:10 +0200

    Converting to switch-based multi-line docker run command

--- .github/actions/setup-sql-server/Initialize-SqlServer.ps1
+++ .github/actions/setup-sql-server/Initialize-SqlServer.ps1
@@ -5,11 +5,15 @@ if ($Env:RUNNER_OS -eq "Windows")
 else
 {
     docker pull mcr.microsoft.com/mssql/server &&
-    docker run `
-        --name sql2019 `
-        --env 'ACCEPT_EULA=Y' `
-        --env 'SA_PASSWORD=Password1!' `
-        --publish 1433:1433 `
-        --detach 'mcr.microsoft.com/mssql/server:2019-latest' &&
+
+    $dockerRunSwitches = @{
+        name = sql2019
+        env = 'ACCEPT_EULA=Y'
+        env = 'SA_PASSWORD=Password1!'
+        publish = 1433:1433
+        detach = 'mcr.microsoft.com/mssql/server:2019-latest'
+    }
+    docker run @dockerRunSwitches &&
+
     docker exec --user 0 sql2019 bash -c 'mkdir /data; chmod 777 /data --recursive; chown mssql:root /data'
 }
