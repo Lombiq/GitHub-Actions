7e11c230d798ebdfb034c8899db12a6e906c29fc
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-06 16:53:29 +0200

    Again

--- .github/actions/setup-sql-server/Initialize-SqlServer.ps1
+++ .github/actions/setup-sql-server/Initialize-SqlServer.ps1
@@ -2,16 +2,17 @@ if ($Env:RUNNER_OS -eq "Windows")
 {
     choco install sql-server-express --no-progress
 }
-else
+els
 {
     # For whatever reason, except name, the switches need to only include a single dash despite them needing a double
     # dash when executing directly from the command line.
-    $dockerRunSwitches = @{
-        '--name' = 'sql2019'
-        '-env' = 'ACCEPT_EULA=Y SA_PASSWORD=Password1!'
-        '-publish' = '1433:1433'
-        '-detach' = 'mcr.microsoft.com/mssql/server:2019-latest'
-    }
+    $dockerRunSwitches = @(
+        '--name', 'sql2019'
+        '--env', 'ACCEPT_EULA=Y'
+        '--env', 'SA_PASSWORD=Password1!'
+        '--publish', '1433:1433'
+        '--detach', 'mcr.microsoft.com/mssql/server:2019-latest'
+    )
 
     docker pull mcr.microsoft.com/mssql/server &&
     docker run @dockerRunSwitches &&
