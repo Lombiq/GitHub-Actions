172fa52c93b98a5ecfd68673402385a9ff0b8c10
Benedek Farkas <benedek.farkas@lombiq.com>
2022-10-14 12:19:13 +0200

    Testing PS module imports

--- .github/actions/verify-submodule-pull-request/Check-Current.ps1
+++ .github/actions/verify-submodule-pull-request/Check-Current.ps1
@@ -1,9 +1,5 @@
 param([string] $Title)
 
-$env:PSModulePath -split ";"
-
-Get-Module -ListAvailable
-
 if (Confirm-PullRequestTitle $Title) { exit 0 }
 Set-Failed ('The pull request title is not in the expected format. Please start with the issue code followed by a ' +
     'colon and the title, e.g. "PROJ-123: My PR Title".')
