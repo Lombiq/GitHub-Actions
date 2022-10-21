8933ab3eaf4d0c051e22cec50ed022d269ecaa6d
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-10-21 21:51:00 +0200

    Format commands and add comment

--- .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
+++ .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
@@ -3,9 +3,11 @@ param ($MaxParallelThreads)
 Write-Output "Replacing maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
 
 ($configFiles = Get-ChildItem -Filter xunit.runner.json -Recurse -FollowSymlink -WarningAction Ignore) | ForEach-Object {
-     (Get-Content $_) |
-        ForEach-Object  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} |
-        Set-Content $_
+    # Need the parentheses to close the file after reading. Without them, you'll receive an error with Set-Content:
+    # "The process cannot access the file '[...]\xunit.runner.json' because it is being used by another process."
+    (Get-Content $_) |
+        ForEach-Object { $_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads" } |
+            Set-Content $_
 }
 
 Write-Output "Replaced $($configFiles.Count) occurrences."
