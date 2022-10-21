9f26474819062aa331dcd8daf2b7dd3b56b6bb42
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-10-21 21:50:09 +0200

    Speed up file search by factor 10000

--- .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
+++ .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
@@ -2,7 +2,7 @@ param ($MaxParallelThreads)
 
 Write-Output "Replacing maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
 
-($configFiles = Get-ChildItem "*/**/xunit.runner.json" -Recurse) | ForEach-Object {
+($configFiles = Get-ChildItem -Filter xunit.runner.json -Recurse -FollowSymlink -WarningAction Ignore) | ForEach-Object {
      (Get-Content $_) |
         ForEach-Object  {$_ -Replace '"maxParallelThreads":\s*([-\d]*)', "`"maxParallelThreads`": $MaxParallelThreads"} |
         Set-Content $_
