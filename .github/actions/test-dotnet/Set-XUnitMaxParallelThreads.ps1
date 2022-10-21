f9dc37318799ef4335351a90cef31e1e48f14b8c
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-10-21 22:31:02 +0200

    Refactor file search command to use parameter splatting

--- .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
+++ .github/actions/test-dotnet/Set-XUnitMaxParallelThreads.ps1
@@ -1,8 +1,19 @@
 param ($MaxParallelThreads)
 
-Write-Output "Replacing maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
+Write-Output "Setting maxParallelThreads in xunit.runner.json files to $MaxParallelThreads."
 
-($configFiles = Get-ChildItem -Filter xunit.runner.json -Recurse -FollowSymlink -WarningAction Ignore) | ForEach-Object {
+$parameters = @{
+        # -Filter is the fastest way to find files because its value is passed directly to the file system API.
+        Filter = "xunit.runner.json"
+        Recurse = $true
+        # -FollowSymLink is needed because without it errors will be thrown of type: 
+        # "Get-ChildItem: The system cannot find the path specified."
+        FollowSymlink = $true
+        # Ignore warnings that will be printed due to multiple symlinks pointing to the same physical file.
+        WarningAction = "Ignore"
+}
+
+($configFiles = Get-ChildItem @parameters) | ForEach-Object {
     # Need the parentheses to close the file after reading. Without them, you'll receive an error with Set-Content:
     # "The process cannot access the file '[...]\xunit.runner.json' because it is being used by another process."
     (Get-Content $_) |
