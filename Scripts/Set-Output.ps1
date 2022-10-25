b5452e6f293a085aa0157ab037d273f85a940f0b
Zoltán Máté <zoltan.mate@lombiq.com>
2022-10-25 15:33:56 +0200

    Refactoring Set-Output function

---
+++ Scripts/Set-Output.ps1
@@ -0,0 +1,11 @@
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]
+    $Key,
+
+    [Parameter(Mandatory = $true, Position = 1)]
+    [string]
+    $Value,
+)
+
+return Write-Output "$Key=$Value" >> $GITHUB_OUTPUT
