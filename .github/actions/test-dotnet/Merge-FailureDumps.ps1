271aa5cd291f6e75114940058d06d811d47c70be
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-02 23:50:09 +0100

    Merge-FailureDumps now supports directory structures without a "test" subfolder

--- .github/actions/test-dotnet/Merge-FailureDumps.ps1
+++ .github/actions/test-dotnet/Merge-FailureDumps.ps1
@@ -1,7 +1,10 @@
-﻿param ($Directory)
+param ($Directory)
 
 New-Item -Type Directory "$Directory/FailureDumps"
-Get-ChildItem "$Directory/test" -Recurse |
+
+$rootDirectory = (Test-Path -Path "$Directory/test") ? "$Directory/test" : $Directory
+
+Get-ChildItem $rootDirectory -Recurse |
     ? { $_.Name -eq 'FailureDumps' } |
     % { $_.GetDirectories() } |
     % { Move-Item $_.FullName "$Directory/FailureDumps/${_.Name}" }
