9ad90347346633bd3fb69e09a08c3a45556d0fce
Márk Lukovics <mark.lukovics@lombiq.com>
2022-11-04 19:39:42 +0100

    Add custom action

---
+++ .github/actions/prefix-pr/index.js
@@ -0,0 +1,39 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const parsePullRequestId = (githubRef) => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId] = result;
+  return pullRequestId;
+};
+
+async function run() {
+  const githubToken = core.getInput("GITHUB_TOKEN");
+  const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
+  const octokit = github.getOctokit(githubToken);
+
+  const pr = await octokit.rest.pulls.get({
+    owner: "Lombiq",
+    repo: "GitHub-Actions",
+    pull_number: pullRequestId,
+  });
+
+  let branch = pr;
+  console.log(pr);
+  let title = pr.data.title;
+  let body = pr.data.body;
+
+  console.log("title", title);
+  console.log("body", body);
+
+  await octokit.rest.pulls.update({
+    owner: "Lombiq",
+    repo: "GitHub-Actions",
+    pull_number: pullRequestId,
+    body: body,
+    title: title,
+  });
+}
+
+run();
