b82510ac4e27070ddd36bdac26e174e201714345
Márk Lukovics <mark.lukovics@lombiq.com>
2022-11-09 15:23:55 +0100

    Changing to PS

---
+++ .github/actions/prefix-pr/GetPR.ps1
@@ -0,0 +1,51 @@
+param(
+    [string] $Github_Repository,
+    [string] $Github_Ref,
+    [string] $Github_Token
+)
+
+$jiraBaseUrl = "https://lombiq.atlassian.net/browse/"
+Write-Output $Github_Repository
+Write-Output "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+Write-Output $Github_Ref
+
+# $url = "https://api.github.com/repos/Lombiq/Github-Actions/pulls/90"
+# $headers = @{"Authorization" = "Bearer $Github_Token"; "Accept" = "application/vnd.github+json"}
+
+# $pr = Invoke-WebRequest $url -Headers $headers | % {$_.Content} | ConvertFrom-Json
+# $title = $pr.title
+# $body = $pr.body
+
+# $originalTitle = $title
+# $originalBody = $body
+
+# $branch = "issue/OSOE-425"
+# if ($branch -NotLike "*issue*") {
+#     Exit
+# }
+
+# $issueKey = $branch.replace("issue/", "")
+# $issueLink = "[$issueKey]($jiraBaseUrl$issuekey)"
+
+# if ($title -NotLike "*$issueKey*") {
+#     $title = $issueKey + ": " + $title
+# }
+
+# if (-Not $body) {
+#     $body = $issueLink
+# }
+# elseif ($body -NotLike "*$issueKey*") {
+#     $body = $issueLink + "`n" + $body
+# }
+# elseif ($body -NotLike "*``[$issueKey``]``($jiraBaseUrl$issuekey``)*") {
+#     $body = $body.replace($issueKey, $issueLink)
+# }
+
+# if (($title -ne $originalTitle) -or ($body -ne $originalBody)) {
+#     $bodyParams = @{"title" = $title; "body" = $body}
+#     Invoke-WebRequest $url -Headers $headers -Method Patch -Body $bodyParams
+# } else {
+#     Write-Output "Nothing was changed"
+# }
+
+# Write-Output "End of file."
