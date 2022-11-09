0b7515edc4ce0775fb6ee89c297aea2df94a1261
Márk Lukovics <mark.lukovics@lombiq.com>
2022-11-09 16:22:11 +0100

    Port complete

--- .github/actions/prefix-pr/Prefix-PR.ps1
+++ .github/actions/prefix-pr/Prefix-PR.ps1
@@ -5,51 +5,42 @@ param(
 )
 
 $jiraBaseUrl = "https://lombiq.atlassian.net/browse/"
-
 $owner, $repo = $Github_Repository.Split('/')
 $prId = $Github_Ref -replace "refs\/pull\/(\d+)\/merge", '$1'
-Write-Output $owner
-Write-Output $repo
-Write-Output $prId
 
+$url = "https://api.github.com/repos/$owner/$repo/pulls/$prId"
+$headers = @{"Authorization" = "Bearer $Github_Token"; "Accept" = "application/vnd.github+json"}
 
-# $url = "https://api.github.com/repos/Lombiq/Github-Actions/pulls/90"
-# $headers = @{"Authorization" = "Bearer $Github_Token"; "Accept" = "application/vnd.github+json"}
+$pr = Invoke-WebRequest $url -Headers $headers | % {$_.Content} | ConvertFrom-Json
+$title = $pr.title
+$body = $pr.body
 
-# $pr = Invoke-WebRequest $url -Headers $headers | % {$_.Content} | ConvertFrom-Json
-# $title = $pr.title
-# $body = $pr.body
+$originalTitle = $title
+$originalBody = $body
 
-# $originalTitle = $title
-# $originalBody = $body
+$branch = "issue/OSOE-425"
+if ($branch -NotLike "*issue*") {
+    Exit
+}
 
-# $branch = "issue/OSOE-425"
-# if ($branch -NotLike "*issue*") {
-#     Exit
-# }
+$issueKey = $branch.replace("issue/", "")
+$issueLink = "[$issueKey]($jiraBaseUrl$issuekey)"
 
-# $issueKey = $branch.replace("issue/", "")
-# $issueLink = "[$issueKey]($jiraBaseUrl$issuekey)"
+if ($title -NotLike "*$issueKey*") {
+    $title = $issueKey + ": " + $title
+}
 
-# if ($title -NotLike "*$issueKey*") {
-#     $title = $issueKey + ": " + $title
-# }
+if (-Not $body) {
+    $body = $issueLink
+}
+elseif ($body -NotLike "*$issueKey*") {
+    $body = $issueLink + "`n" + $body
+}
+elseif ($body -NotLike "*``[$issueKey``]``($jiraBaseUrl$issuekey``)*") {
+    $body = $body.replace($issueKey, $issueLink)
+}
 
-# if (-Not $body) {
-#     $body = $issueLink
-# }
-# elseif ($body -NotLike "*$issueKey*") {
-#     $body = $issueLink + "`n" + $body
-# }
-# elseif ($body -NotLike "*``[$issueKey``]``($jiraBaseUrl$issuekey``)*") {
-#     $body = $body.replace($issueKey, $issueLink)
-# }
-
-# if (($title -ne $originalTitle) -or ($body -ne $originalBody)) {
-#     $bodyParams = @{"title" = $title; "body" = $body}
-#     Invoke-WebRequest $url -Headers $headers -Method Patch -Body $bodyParams
-# } else {
-#     Write-Output "Nothing was changed"
-# }
-
-# Write-Output "End of file."
+if (($title -ne $originalTitle) -or ($body -ne $originalBody)) {
+    $bodyParams = @{"title" = $title; "body" = $body}
+    Invoke-WebRequest $url -Headers $headers -Method Patch -Body $bodyParams
+}
