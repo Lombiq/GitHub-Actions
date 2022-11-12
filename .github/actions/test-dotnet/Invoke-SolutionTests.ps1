51dfe31a05d3ea7841e0d61b0c081009d2a68c3c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-12 18:02:02 +0100

    Not writing test outputs to a file, to be able to troubleshoot tests timing out

--- .github/actions/test-dotnet/Invoke-SolutionTests.ps1
+++ .github/actions/test-dotnet/Invoke-SolutionTests.ps1
@@ -35,7 +35,7 @@ $tests = dotnet sln list |
     }
 
 foreach ($test in $tests) {
-    dotnet test -c Release --no-restore --no-build --nologo --logger "trx;LogFileName=test-results.trx" --verbosity $Verbosity $test 2>&1 >test.out
+    dotnet test -c Release --no-restore --no-build --nologo --logger "trx;LogFileName=test-results.trx" --verbosity $Verbosity $test
 
     if ($?)
     {
