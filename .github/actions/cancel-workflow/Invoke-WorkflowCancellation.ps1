e30a5367cde60a0cc1e9c108fda416e99cdbb143
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-16 02:03:43 +0100

    Files don't work, let's try curl

--- .github/actions/cancel-workflow/Invoke-WorkflowCancellation.ps1
+++
@@ -1,6 +0,0 @@
-param ($Repository, $RunId)
-
-Write-Output "::error::Canceling workflow due to one of the jobs failing."
-$url = "https://api.github.com/repos/$Repository/actions/runs/$RunId/cancel"
-$headers = @{"Authorization" = "Bearer $($Env:GITHUB_TOKEN)"; "Accept" = "application/vnd.github+json"}
-Invoke-WebRequest $url -Headers $headers -Method Post
