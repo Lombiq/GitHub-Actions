68f90190f802b053d8a1eb14d085a4dd7ecf54c6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-18 01:42:31 +0100

    Adding msbuild action and workflow

---
+++ .github/actions/msbuild/Build-DotNetFrameworkSolutionOrProject.ps1
@@ -0,0 +1,63 @@
+param (
+    [string] $SolutionOrProject,
+    [string] $Verbosity,
+    [string] $EnableCodeAnalysis,
+    [string] $Version,
+    [string] $Switches)
+
+function ConvertTo-Array([string] $rawInput)
+{
+    $rawInput.Replace("`r", "").Split("`n") | ForEach-Object { $_.Trim() } | Where-Object { $_ }
+}
+
+Write-Output ".NET version number: $Version"
+
+if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj)
+{
+    Write-Output "::group::Gulp Extensions found. It needs to be explicitly built before the solution."
+
+    # These need to be different than that of msbuild.
+    $gulpBuildSwitches = ConvertTo-Array @"
+    --configuration:Release
+    --nologo
+    --verbosity:$Verbosity
+    --warnaserror
+    --warnAsMessage:MSB3026
+    --consoleLoggerParameters:NoSummary
+    -p:TreatWarningsAsErrors=true
+    -p:RunAnalyzersDuringBuild=$EnableCodeAnalysis
+    -p:Retries=4
+    -p:RetryDelayMilliseconds=1000
+    -p:Version=$Version
+"@
+
+    $startTime = [DateTime]::Now
+    dotnet build src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj @gulpBuildSwitches
+    $endTime = [DateTime]::Now
+
+    Write-Output ("Gulp Extensions build took {0:0.###} seconds." -f ($endTime - $startTime).TotalSeconds)
+    Write-Output "::endgroup::"
+}
+
+# Notes on build switches that aren't self-explanatory:
+# - -p:Retries and -p:RetryDelayMilliseconds are to retry builds if it fails the first time due to random locks.
+
+
+$buildSwitches = ConvertTo-Array @"
+    -p:Configuration=Release
+    -restore
+    --verbosity:$Verbosity
+    --warnaserror
+    -p:TreatWarningsAsErrors=true
+    -p:RunAnalyzersDuringBuild=$EnableCodeAnalysis
+    -p:Retries=4
+    -p:RetryDelayMilliseconds=1000
+    -p:Version=$Version
+    $Switches
+"@
+
+Write-Output "Building solution or project with ``msbuild $SolutionOrProject $($buildSwitches -join " ")``."
+
+msbuild $SolutionOrProject @buildSwitches
+
+Close-DotNetBuildServers
