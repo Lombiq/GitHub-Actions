f9f0470b708b96938e3455bcbdaf12f8adb6571e
Zoltán Máté <zoltan.mate@lombiq.com>
2022-11-24 17:06:46 +0100

    Post merge fixing

--- .github/actions/test-dotnet/Merge-FailureDumps.ps1
+++ .github/actions/test-dotnet/Merge-FailureDumps.ps1
@@ -1,22 +1,16 @@
-﻿param ($Directory)
-
-
+param ($Directory)
 
 New-Item -Type Directory "$Directory/FailureDumps"
 
-
-
 $testDirectory = "$Directory/test"
-
 $rootDirectory = (Test-Path -Path $testDirectory) ? $testDirectory : $Directory
 
-
-
 Get-ChildItem $rootDirectory -Recurse |
-
     Where-Object { $PSItem.Name -eq 'FailureDumps' } |
-
     ForEach-Object { $PSItem.GetDirectories() } |
-
     ForEach-Object { Move-Item $PSItem.FullName "$Directory/FailureDumps/${_.Name}" }
 
+Get-ChildItem $rootDirectory -Recurse |
+    Where-Object { $PSItem.Name -eq 'Temp' } |
+    ForEach-Object { $PSItem.GetDirectories() } |
+    ForEach-Object { Move-Item $PSItem.FullName "$Directory/Temp/${_.Name}" }
