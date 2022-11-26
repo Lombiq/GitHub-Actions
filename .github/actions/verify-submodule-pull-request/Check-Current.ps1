d1d171cf5cdb1eaf24dd25023164b9a8626dc2bb
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-26 21:19:26 +0100

    Removing unused script

--- .github/actions/verify-submodule-pull-request/Check-Current.ps1
+++
@@ -1,5 +0,0 @@
-param([string] $Title)
-
-if (Confirm-PullRequestTitle $Title) { exit 0 }
-Set-Failed ('The pull request title is not in the expected format. Please start with the issue code followed by a ' +
-    'colon and the title, e.g. "PROJ-123: My PR Title".')
