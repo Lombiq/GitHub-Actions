db5ccec3bbd001e5ca7873852a65521ec4dd2a0e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-26 16:46:14 +0100

    Moving PR label check to its own action

--- .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
+++ .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
@@ -1,16 +1,6 @@
-<#
-  .SYNOPSIS
-  Tests if the pull request in the given repository, with the given number, has any of the two provided labels.
+# We need to fetch the PR details from the API as opposed to just using the context, because a label added after the
+# start of the run wouldn't be present in it.
 
-  .DESCRIPTION
-  We need to fetch the PR details from the API as opposed to just using the context, because a label added after the
-  start of the run wouldn't be present in it.
-
-  .OUTPUTS
-  Sets a boolean in the "contains-label" GitHub output variable.
-#>
-
-# This could support any number of labels in the future, but right now, we only need exactly two.
 param($Repository, $PullRequestNumber, $Label1, $Label2)
 
 $url = "https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}"
