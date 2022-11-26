cfd64963b2b594c56c8035618d386ec87c75d773
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-26 21:58:45 +0100

    Adding missing script

---
+++ .github/actions/auto-resolve-done-jira-issue/Initialize-IssueParameters.ps1
@@ -0,0 +1,20 @@
+param($Repository, $PullRequestNumber, $IsDone, $IsResolve)
+
+# We need to fetch the PR details from the API as opposed to just using the context, because the title may be changed
+# (by the user or the add-jira-issue-code-to-pull-request action) after the start of the run and that wouldn't be
+# present in it.
+$url = "https://api.github.com/repos/$Repository/pulls/$PullRequestNumber"
+$response = Invoke-WebRequest $url -Headers (Get-GitHubApiAuthorizationHeader) -Method Get
+$content = $response | ConvertFrom-Json
+
+Set-GitHubOutput 'key' (Get-JiraIssueKeyFromPullRequestTitle $content.title)
+
+if ($IsDone) $transition = 'Done'
+elseif ($IsResolve) $transition = 'Resolve'
+else $transition = ''
+
+Set-GitHubOutput 'transition' $transition
+
+Write-Output $IsDone
+Write-Output $IsResolve
+Write-Output $transition
