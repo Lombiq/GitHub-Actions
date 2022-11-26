ed9a3bddd5cd39c8c3303eba67fbbc72cbfb9b89
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-26 02:25:21 +0100

    Removing non-working check

--- .github/actions/auto-merge-pull-request/Test-Mergeability.ps1
+++ .github/actions/auto-merge-pull-request/Test-Mergeability.ps1
@@ -1,12 +1,7 @@
 param ($Response)
 
-if ($Response.StatusCode -ne 200)
-{
-    Write-Output "Getting the pull request details failed with HTTP $($Response.StatusCode)."
-    exit 1
-}
-
 $content = $Response | ConvertFrom-Json
+
 $labelFound = $content.labels.Where(
     {
         $PSItem.name -eq 'merge-if-checks-succeed' -or $PSItem.name -eq 'merge-and-resolve-jira-issue-if-checks-succeed'
