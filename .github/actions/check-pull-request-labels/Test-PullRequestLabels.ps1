1153e8df617773d3f1df6b830bb97f776cedb8a6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-26 21:56:16 +0100

    Code styling

--- .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
+++ .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
@@ -9,4 +9,4 @@ $content = $response | ConvertFrom-Json
 
 $labelFound = $content.labels.Where({ $PSItem.name -eq $Label1 -or $PSItem.name -eq $Label2 }, 'First').Count -gt 0
 
-Set-GitHubOutput "contains-label" $labelFound
+Set-GitHubOutput 'contains-label' $labelFound
