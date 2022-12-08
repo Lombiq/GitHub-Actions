ed890f8540d392ad14615f580ba9f6e5fa3a24c1
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-08 21:12:07 +0100

    add-azure-application-insights-release-annotation action

---
+++ .github/actions/add-azure-application-insights-release-annotation/Add-ReleaseAnnotation.ps1
@@ -0,0 +1,16 @@
+param(
+    [parameter(Mandatory = $true)][string]$ApplicationInsightsResourceId,
+    [parameter(Mandatory = $true)][string]$ReleaseName,
+    [parameter(Mandatory = $false)]$ReleaseProperties = @()
+)
+
+$annotation = @{
+    Id = [GUID]::NewGuid();
+    AnnotationName = $ReleaseName;
+    EventTime = (Get-Date).ToUniversalTime().GetDateTimeFormats("s")[0];
+    # AI only displays annotations from the "Deployment" category so this must be this string.
+    Category = "Deployment";
+    Properties = ConvertTo-Json $ReleaseProperties -Compress
+}
+
+Invoke-AzRestMethod -Path "$ApplicationInsightsResourceId/Annotations?api-version=2015-05-01" -Method PUT -Payload $body
