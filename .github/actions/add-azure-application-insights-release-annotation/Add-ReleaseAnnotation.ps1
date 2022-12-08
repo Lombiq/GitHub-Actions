92cfc210d628a7a3964f766d46620809cf03218e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-08 21:58:06 +0100

    PS syntax fix

--- .github/actions/add-azure-application-insights-release-annotation/Add-ReleaseAnnotation.ps1
+++ .github/actions/add-azure-application-insights-release-annotation/Add-ReleaseAnnotation.ps1
@@ -21,5 +21,5 @@ $response = Invoke-AzRestMethod -Path "$ApplicationInsightsResourceId/Annotation
 if ($response.StatusCode -ne 200)
 {
     Write-Error "Adding the release annotation failed with the status code $($response.StatusCode)."
-    Write-Output response
+    Write-Output $response
 }
