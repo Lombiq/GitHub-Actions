cea81a1091b13b1b96a02b160af9e16e72ec67ea
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-12 19:52:51 +0100

    Centralizing path matching logic, and adding better error message

---
+++ Scripts/Get-SolutionOrProjectPath.ps1
@@ -0,0 +1,22 @@
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string] $PathPattern
+)
+
+$matchedItems = Get-ChildItem $PathPattern
+$matchCount = ($matchedItems | Measure-Object).Count
+
+if ($matchCount -ne 1)
+{
+    $errorMessage =
+        "The solution or project path pattern $PathPattern matches $matchCount items, see below. Fix the " +
+        "pattern so it matches exactly one file."
+
+    Write-Error $errorMessage
+
+    $matchedItems.FullName
+
+    exit 1
+}
+
+$matchedItems.FullName
