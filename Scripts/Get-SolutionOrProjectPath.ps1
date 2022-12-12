6a5ccd85a9f73cc31215e9e2fc923b626c56ab93
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-12 20:03:16 +0100

    Better error message if no files are matched

--- Scripts/Get-SolutionOrProjectPath.ps1
+++ Scripts/Get-SolutionOrProjectPath.ps1
@@ -9,10 +9,16 @@ $matchCount = ($matchedItems | Measure-Object).Count
 if ($matchCount -ne 1)
 {
     $errorMessage =
-        "The solution or project path pattern `"$PathPattern`" matches $matchCount items, see below. Fix the " +
-        "pattern so it matches exactly one file.
+        "The solution or project path pattern `"$PathPattern`" matches $matchCount items, see below. Fix the pattern " +
+        "so it matches exactly one file.
 $($matchedItems | Select-Object $PSItem.Name)"
 
+    if ($matchCount -eq 0)
+    {
+        $errorMessage =
+            "The solution or project path pattern `"$PathPattern`" matches no items. Fix the pattern so it matches " +
+            "exactly one file."
+    }
 
     Write-Error $errorMessage
 
