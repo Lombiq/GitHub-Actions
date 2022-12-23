0f504407df1ee0d3cf5315f010ecb2c75e40cf98
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-23 18:41:24 +0100

    More debug output

--- .github/actions/verify-submodule-pull-request/Check-Parent.ps1
+++ .github/actions/verify-submodule-pull-request/Check-Parent.ps1
@@ -4,7 +4,8 @@ param(
 )
 
 Write-Output "Header:"
-Write-Output Get-GitHubApiAuthorizationHeader
+Write-Output (Get-GitHubApiAuthorizationHeader)
+Write-Output (Get-GitHubApiAuthorizationHeader).Accept
 
 $url = "https://api.github.com/repos/Lombiq/GitHub-Actions/pulls/159"
 $response = Invoke-WebRequest $url -Headers (Get-GitHubApiAuthorizationHeader) -Method Get
