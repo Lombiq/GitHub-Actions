1110c3fa03de45931e994f4b02f894f77b4ec9d9
Benedek Farkas <benedek.farkas@lombiq.com>
2023-01-09 11:22:25 +0100

    Suppressing PSReviewUnusedParameter for some of the parameters in Test-PullRequestLabels

--- .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
+++ .github/actions/check-pull-request-labels/Test-PullRequestLabels.ps1
@@ -1,6 +1,14 @@
 # We need to fetch the PR details from the API as opposed to just using the context because a label added after the
 # start of the run wouldn't be present in it.
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter',
+    'Label1',
+    Justification = 'False positive due to https://github.com/PowerShell/PSScriptAnalyzer/issues/1472.')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter',
+    'Label2',
+    Justification = 'False positive due to https://github.com/PowerShell/PSScriptAnalyzer/issues/1472.')]
 param($Repository, $PullRequestNumber, $Label1, $Label2)
 
 # See https://cli.github.com/manual/gh_pr_view
