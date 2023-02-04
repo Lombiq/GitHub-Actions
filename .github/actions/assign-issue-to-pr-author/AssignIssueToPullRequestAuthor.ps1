44e6a437301440e90f0eecfd23c7585667326350
Dorra Jaouad <dorra.jaouad@lombiq.com>
2023-02-04 19:52:33 +0100

    Debugging

--- .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
+++ .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
@@ -1,10 +1,12 @@
 param(
-    [string] $IssueQuery,
+    [string] $Body,
     [string] $Assignee,
     [string] $GitHubRepository,
     [string] $PullRequestID
 )
 
+$Jira_Key = if ("$Body" -match '\[(.*?)\]') { $Matches[1] } else { "No_Key_added" }
+$IssueQuery = "$Jira_Key in:title"
 $output = gh issue list --search $IssueQuery --repo $GitHubRepository
 $firstItem = ($output | Select-Object -First 1).number
 
