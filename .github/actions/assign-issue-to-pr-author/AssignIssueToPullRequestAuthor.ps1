31455bdddff7db19d4f778ab6f57d0f59b2d6d3f
Dorra Jaouad <dorra.jaouad@lombiq.com>
2023-02-04 20:01:26 +0100

    Debugging

--- .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
+++ .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
@@ -8,7 +8,9 @@ param(
 $Jira_Key = if ("$Body" -match '\[(.*?)\]') { $Matches[1] } else { "No_Key_added" }
 $IssueQuery = "$Jira_Key in:title"
 $output = gh issue list --search $IssueQuery --repo $GitHubRepository
-$firstItem = ($output | Select-Object -First 1).number
+Write-Output $output
+$firstItem = ($output | Select-Object -First 1)
+Write-Output $firstItem
 
 if ($firstItem) {
     gh api -X PATCH "/repos/$GitHubRepository/issues/$firstItem.number" -f "assignees=$Assignee"
