6873afa20b2eadce5a25cea48dd0c5b1ecbec85e
Dorra Jaouad <dorra.jaouad@lombiq.com>
2023-02-05 20:03:35 +0100

    Code Styling

--- .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
+++ .github/actions/assign-issue-to-pr-author/AssignIssueToPullRequestAuthor.ps1
@@ -1,19 +1,27 @@
 param(
     [string] $Body,
     [string] $Assignee,
-    [string] $GitHubRepository,
-    [string] $PullRequestID
+    [string] $GitHubRepository
 )
 
-$Jira_Key = if ("$Body" -match '\[(.*?)\]') { $Matches[1] } else { "No_Key_added" }
+$Jira_Key = if ("$Body" -match '\[(.*?)\]') {
+    $Matches[1]
+} else { 
+    "No_Key_added" 
+}
+
 $IssueQuery = "$Jira_Key in:title"
-$output = gh issue list --search $IssueQuery --repo $GitHubRepository
-Write-Output $output
-$firstItem = ($output | Select-Object -First 1)
-Write-Output $firstItem
-$issueNumber = $firstItem -split "\t" | Select-Object -First 1
-if ($firstItem) {
-    gh api -X PATCH "/repos/$GitHubRepository/issues/$issueNumber" -f "assignee=$Assignee"
+$Output = gh issue list --search $IssueQuery --repo $GitHubRepository
+
+Write-Output $Output
+
+$FirstItem = ($Output | Select-Object -First 1)
+Write-Output $FirstItem
+
+$IssueNumber = $FirstItem -split "\t" | Select-Object -First 1
+
+if ($FirstItem) {
+    gh api -X PATCH "/repos/$GitHubRepository/issues/$IssueNumber" -f "assignee=$Assignee"
 } else {
     Write-Output "No issue was found with the query '$IssueQuery' in the repository '$GitHubRepository'"
 }
