66190eaa23ebe212794a591197dd5bb22266fc1e
József Horváth <jozsef.horvath@lombiq.com>
2023-02-23 22:15:30 +0100

    Fixing dump merge

--- .github/actions/test-dotnet/Merge-BlameHangDumps.ps1
+++ .github/actions/test-dotnet/Merge-BlameHangDumps.ps1
@@ -4,5 +4,5 @@ New-Item -Type Directory "$Directory/BlameHangDumps"
 
 Get-ChildItem $Directory -Recurse |
     Where-Object { ($PSItem.Name -like 'Sequence_*.xml') -or ($PSItem.Name -like '*_hangdump.dmp') } |
-    ForEach-Object { $PSItem.GetDirectories() } |
+    ForEach-Object { $PSItem.Directory } |
     ForEach-Object { Move-Item $PSItem.FullName "$Directory/BlameHangDumps/${_.Name}" }
