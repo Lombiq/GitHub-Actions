5a7d4cc36a73467340104ef4de056ea96d52f488
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-04-04 20:02:08 +0200

    Trying to fix PS styling

--- .github/actions/test-dotnet/Merge-BlameHangDumps.ps1
+++ .github/actions/test-dotnet/Merge-BlameHangDumps.ps1
@@ -42,13 +42,12 @@ Get-ChildItem $testDirectory.Path -Recurse |
             New-Item -Type Directory -Path $destinationDirectory
         }
 
-        Get-ChildItem -Path $PSItem.FullName -Recurse |
-            ForEach-Object {
-                if ($PSItem.Name -like '*:*')
-                {
-                    $newName = $PSItem.Name -replace ':', '_'
-                    Rename-Item -Path $PSItem.FullName -NewName $newName
-                }
-                Copy-Item -Path $PSItem.FullName -Destination $destinationDirectory
+        Get-ChildItem -Path $PSItem.FullName -Recurse | ForEach-Object {
+            if ($PSItem.Name -like '*:*')
+            {
+                $newName = $PSItem.Name -replace ':', '_'
+                Rename-Item -Path $PSItem.FullName -NewName $newName
             }
+            Copy-Item -Path $PSItem.FullName -Destination $destinationDirectory
+        }
     }
