2d68f074f6541989b4cf3a999d60ecc8e8e9d360
David Puplava <dpuplava@lavatg.com>
2023-07-19 22:18:06 -0500

    Refactor GitHub Actions ref check into reusable action
    Conslidate GitHub Actions (GHA) ref check logic into reusable action for
    use by other workflows. Moved PowerShell script into our .ps1 file
    consistent with other actions in repo. Changed action input names to
    better describe their purpose and form.

---
+++ .github/actions/verify-gha-refs/Check-Called-GHA-Refs.ps1
@@ -0,0 +1,40 @@
+param(
+    [String[]] $CalledRepoBaseIncludeList, 
+    [String[]] $PathIncludeList,
+    [String[]] $FileIncludeList,
+    [String] $ExpectedRef,
+    [String] $GitHubRepository,
+    [String] $GitHubRefName
+)
+
+$CalledRepoBaseIncludeList = $CalledRepoBaseIncludeList.ForEach({ "uses:\s*" + $_ })
+
+$mismatchRefs = Get-ChildItem -Path $PathIncludeList -Include $FileIncludeList -Force -Recurse | 
+Select-String -Pattern $CalledRepoBaseIncludeList | 
+Select-String -Pattern $ExpectedRef -NotMatch
+
+if ($mismatchRefs.Count -gt 0)
+{
+    "These called worflows and actions do not match expected ref '$ExpectedRef'." >> $env:GITHUB_STEP_SUMMARY
+
+    foreach ($mismatch in $mismatchRefs) 
+    {
+        $filename = $mismatch.RelativePath($pwd)
+        $linenumber = $mismatch.LineNumber
+        $title = $mismatch.Line
+
+        # The below statement won't work becuase actions/toolkit will not link to files that are not part of the commit.
+        # See: https://github.com/actions/toolkit/issues/470
+        # Write-Output "::error file=$filename,line=$linenumber,title=$title::GHA Ref does not match '${{ inputs.expected-ref }}'" 
+
+        # As a workaround, link directly to file.
+        "- <a href='https://github.com/$GitHubRepository/blob/$GitHubRefName/$filename#L$linenumber'>$filename#L$linenumber</a>" >> $env:GITHUB_STEP_SUMMARY
+
+        # Beware the backtick character is the powershell escape character.
+        "``````yaml" >> $env:GITHUB_STEP_SUMMARY
+        "$title" >> $env:GITHUB_STEP_SUMMARY
+        "``````" >> $env:GITHUB_STEP_SUMMARY
+    }
+
+    exit 1
+}
