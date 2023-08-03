4e04e72852f96450d704d390f9ff4e3e9f35a0fc
David Puplava <dpuplava@lavatg.com>
2023-08-02 23:39:08 -0500

    Fix PowerShell static code analysis errors

--- .github/actions/get-changed-gha-items/Get-GHA-Items-From-File-List.ps1
+++ .github/actions/get-changed-gha-items/Get-GHA-Items-From-File-List.ps1
@@ -3,24 +3,26 @@ param(
 )
 
 # Filter actions based on files in action directory.
-$actionFiles = $FileIncludeList | Where-Object -FilterScript { 
-    (Get-Item $PSitem).Directory.GetFiles("action.yml").Count -gt 0 -or
-    (Get-Item $PSitem).Directory.GetFiles("action.yaml").Count -gt 0
+$actionFiles = $FileIncludeList | Where-Object -FilterScript
+{
+    (Get-Item $PSitem).Directory.GetFiles('action.yml').Count -gt 0 -or
+    (Get-Item $PSitem).Directory.GetFiles('action.yaml').Count -gt 0
 }
 
 # GitHub Actions are called by directory name. Get directory and de-duplicate list.
-$actions = $actionFiles.ForEach({ $PSItem.Replace("/" + $(Get-Item $PSItem).Name, "") }) | select -Unique
+$actions = $actionFiles.ForEach({ $PSItem.Replace('/' + $(Get-Item $PSItem).Name, '') }) | Select-Object -Unique
 
 # Filter workflow files excluding action yaml file names.
-$workflows = $FileIncludeList | Where-Object -FilterScript { 
-    (Get-Item $PSitem).BaseName -ne "action" -and 
-        ((Get-Item $PSitem).Extension -eq ".yml" -or 
-         (Get-Item $PSitem).Extension -eq ".yaml")
+$workflows = $FileIncludeList | Where-Object -FilterScript
+{
+    (Get-Item $PSitem).BaseName -ne 'action' -and
+        ((Get-Item $PSitem).Extension -eq '.yml' -or
+         (Get-Item $PSitem).Extension -eq '.yaml')
 }
 
 # Combine actions and workflows.
 $items = $actions + $workflows
 
-$output = "changed-items=@(" + $($items | Join-String -DoubleQuote -Separator ',') + ")"
+$output = 'changed-items=@(' + $($items | Join-String -DoubleQuote -Separator ',') + ')'
 Write-Output "output=$output"
 $output >> $env:GITHUB_OUTPUT
