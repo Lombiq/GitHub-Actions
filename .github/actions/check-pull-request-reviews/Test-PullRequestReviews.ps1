7f7da0d573f7f77e33226399f16c5abd92e5c414
David Puplava <dpuplava@lavatg.com>
2023-08-10 22:52:53 -0500

    Remove unused code that counts PR review states

--- .github/actions/check-pull-request-reviews/Test-PullRequestReviews.ps1
+++ .github/actions/check-pull-request-reviews/Test-PullRequestReviews.ps1
@@ -5,13 +5,6 @@ param($Repository, $PullRequestNumber)
 
 # See https://cli.github.com/manual/gh_pr_view
 $content = gh api "repos/$Repository/pulls/$PullRequestNumber/reviews" | ConvertFrom-Json
-
-$approvedCount = $content.Where({ $PSItem.state -eq 'APPROVED' }).Count
-$requestChangesCount = $content.Where({ $PSItem.state -eq 'CHANGES_REQUESTED' }).Count
-$commentCount = $content.Where({ $PSItem.state -eq 'COMMENTED' }).Count
 $lastReviewApproved = ($content | Select-Object -Last 1).state -eq 'APPROVED'
 
-Set-GitHubOutput 'approved-count' $approvedCount
-Set-GitHubOutput 'request-changes-count' $requestChangesCount
-Set-GitHubOutput 'comment-count' $commentCount
 Set-GitHubOutput 'last-review-approved' $lastReviewApproved
