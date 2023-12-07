75a2b2164739bb549eb179892791d2a29869efa9
Roland Márkus <roland.markus@lombiq.com>
2023-12-07 19:29:59 +0100

    Dry and renaming.

--- .github/actions/setup-sql-server/Initialize-SqlServer.ps1
+++ .github/actions/setup-sql-server/Initialize-SqlServer.ps1
@@ -1,20 +1,23 @@
+$sqlServerVersion = '2022'
+
 if ($Env:RUNNER_OS -eq 'Windows')
 {
-    choco install sql-server-2022 --no-progress
+    choco install sql-server-$sqlServerVersion --no-progress
 }
 else
 {
-    $sqlServerName = 'sql2022'
-    $sqlServerVersion = '2022-latest'
+    $sqlServerName = "uitt-sqlserver"
+    $sqlServerLink = "mcr.microsoft.com/mssql/server:${sqlServerVersion}-latest"
+    
     $dockerRunSwitches = @(
         '--name', $sqlServerName
         '--env', 'ACCEPT_EULA=Y'
         '--env', 'SA_PASSWORD=Password1!'
         '--publish', '1433:1433'
-        '--detach', "mcr.microsoft.com/mssql/server:$sqlServerVersion"
+        '--detach', $sqlServerLink
     )
 
-    docker pull "mcr.microsoft.com/mssql/server:$sqlServerVersion" &&
+    docker pull $sqlServerLink &&
     docker run @dockerRunSwitches &&
     docker exec --user 0 $sqlServerName bash -c 'mkdir /data; chmod 777 /data --recursive; chown mssql:root /data'
 }
