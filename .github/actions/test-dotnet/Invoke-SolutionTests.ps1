a06d50c0e578223571c36d435f9262e0d322aced
Roland Márkus <roland.markus@lombiq.com>
2023-12-19 11:14:20 +0100

    Using local instead of MSSQLSERVER.

--- .github/actions/test-dotnet/Invoke-SolutionTests.ps1
+++ .github/actions/test-dotnet/Invoke-SolutionTests.ps1
@@ -16,7 +16,7 @@ $connectionStringSuffix = @(
 ) -join ''
 if ($Env:RUNNER_OS -eq 'Windows')
 {
-    $connectionStringStem = 'Server=.\MSSQLSERVER;Database=LombiqUITestingToolbox_{{id}};Integrated Security=True'
+    $connectionStringStem = 'Server=.\(local);Database=LombiqUITestingToolbox_{{id}};Integrated Security=True'
 }
 else
 {
