84b047978d66ebb627fa379cad4bb0f5d0b98736
Krisztián Németh <krisztian.nemeth@lombiq.com>
2024-02-06 11:22:29 +0100

    Testing

--- .github/actions/add-azure-application-insights-release-annotation/Add-ReleaseAnnotation.ps1
+++ .github/actions/add-azure-application-insights-release-annotation/Add-ReleaseAnnotation.ps1
@@ -7,28 +7,25 @@ param(
 Write-Output "Adding release annotation with the release name `"$ReleaseName`"."
 
 $annotation = @{
-    Id = [Guid]::NewGuid()
-    AnnotationName = $ReleaseName
-    EventTime = (Get-Date).ToUniversalTime().GetDateTimeFormats('s')[0]
-    # AI only displays annotations from the "Deployment" category so this must be this string.
-    Category = 'Deployment'
-    #Properties = ConvertTo-Json $ReleaseProperties -Compress
+    Id = [GUID]::NewGuid();
+    AnnotationName = $ReleaseName;
+    EventTime = (Get-Date).ToUniversalTime().GetDateTimeFormats("s")[0];
+    Category = "Deployment"; #Application Insights only displays annotations from the "Deployment" Category
+    Properties = ConvertTo-Json $ReleaseProperties -Compress
 }
 
-# $headers = @{
-#     "Content-Type" = "application/json"
-# }
-
 # Encoding parenthesis to prevent the request from failing.
-$ApplicationInsightsResourceId = $ApplicationInsightsResourceId.Replace('(', '%28').Replace(')', '%29')
+#$ApplicationInsightsResourceId = $ApplicationInsightsResourceId.Replace('(', '%28').Replace(')', '%29')
 $body = (ConvertTo-Json $annotation -Compress) -replace '(\\+)"', '$1$1"' -replace "`"", "`"`""
 
-Write-Output "Running following command:"
-Write-Output "az rest --method put --uri ""$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01"" --debug"
-az rest --method put --uri "$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01" --debug
+az rest --method put --uri "$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01" --body "$($body) " --debug
 
-#Write-Output "Invoke-AzRestMethod -Path ""$ApplicationInsightsResourceId/Annotations?api-version=2015-05-01"" -Method PUT -Payload $body"
-#Invoke-AzRestMethod -Path "$ApplicationInsightsResourceId/Annotations?api-version=2015-05-01" -Method PUT -Payload $body 
+az rest --method put --uri "$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01" --body "$($body) " --headers "Content-Type=application/json" --debug
+
+az rest --method put --uri "$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01" --body "$($body)" --headers "Content-Type=application/json" --debug
+
+az rest --method put --uri "$($ApplicationInsightsResourceId)/Annotations?api-version=2015-05-01" --body "$($body)" --debug
+
 if (!$?)
 {
     exit 1
