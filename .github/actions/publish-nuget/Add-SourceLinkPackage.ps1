cac4291b64e10913f052a5cffd82a4e620a26bea
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-29 00:59:13 +0100

    Use dotnet CLI in Add-SourceLinkPackage.

--- .github/actions/publish-nuget/Add-SourceLinkPackage.ps1
+++ .github/actions/publish-nuget/Add-SourceLinkPackage.ps1
@@ -1,19 +1,3 @@
-# Register NuGet.org as a package source since GitHub runners don't necessarily have it by default.
-$existingSource = Get-PackageSource -Name NuGet.org -ErrorAction SilentlyContinue
-
-if ($existingSource)
-{
-    Write-Output 'Package source for NuGet.org is already registered.'
-}
-else
-{
-    Register-PackageSource -Name NuGet.org -Location https://api.nuget.org/v3/index.json -ProviderName NuGet
-}
-
-# Get the latest version of the package.
-$latestPackage = Find-Package -Name Microsoft.SourceLink.GitHub -Source NuGet.org | Sort-Object Version -Descending | Select-Object -First 1
-$latestVersion = $latestPackage.Version
-
 # Find solution file.
 $solutionFile = Get-ChildItem -Path . -Filter *.sln -Recurse | Select-Object -First 1
 
@@ -33,29 +17,5 @@ else
 
 foreach ($projectFile in $projectFiles)
 {
-    Write-Output "Processing project: $($projectFile.FullName)"
-
-    # Load the project file as XML.
-    $projectXml = [xml](Get-Content -Path $projectFile.FullName)
-
-    # Define the xmlns to access the elements in the csproj.
-    $namespaceManager = New-Object Xml.XmlNamespaceManager $projectXml.NameTable
-    $namespaceManager.AddNamespace('ns', $projectXml.DocumentElement.NamespaceURI)
-
-    # Create a new ItemGroup.
-    $itemGroup = $projectXml.CreateElement('ItemGroup', $projectXml.DocumentElement.NamespaceURI)
-
-    # Create a new PackageReference.
-    $packageNode = $projectXml.CreateElement('PackageReference', $projectXml.DocumentElement.NamespaceURI)
-    $packageNode.SetAttribute('Include', 'Microsoft.SourceLink.GitHub')
-    $packageNode.SetAttribute('Version', $latestVersion)
-
-    # Add the package to the ItemGroup.
-    $itemGroup.AppendChild($packageNode)
-
-    # Add the new ItemGroup to the project.
-    $projectXml.Project.AppendChild($itemGroup)
-
-    # Save the changes back to the .csproj file.
-    $projectXml.Save($projectFile.FullName)
+    dotnet add $projectFile.FullName package 'Microsoft.SourceLink.GitHub' --source 'https://api.nuget.org/v3/index.json'
 }
