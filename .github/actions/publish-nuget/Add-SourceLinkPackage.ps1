d600968b9d9dbd68e6efc53a6815ccaea74e3808
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-04-29 22:00:12 +0200

    Setting NuGetBuild in the XML

--- .github/actions/publish-nuget/Add-SourceLinkPackage.ps1
+++ .github/actions/publish-nuget/Add-SourceLinkPackage.ps1
@@ -17,8 +17,26 @@ else
 
 foreach ($projectFile in $projectFiles)
 {
-    # --no-restore because NuGet restore needs -p:NuGetBuild=true passed but that only works with dotnet restore, what
-    # we run in a separate step.
+    # Load the project file as XML.
+    $projectXml = [xml](Get-Content $projectFile)
+
+    # Define the xmlns to access the elements in the csproj.
+    $ns = New-Object Xml.XmlNamespaceManager $projectXml.NameTable
+    $ns.AddNamespace('ns', $projectXml.DocumentElement.NamespaceURI)
+
+    # Create a new NuGetBuild property in a new PropertyGroup.
+    $propertyGroup = $projectXml.CreateElement('PropertyGroup', $projectXml.DocumentElement.NamespaceURI)
+    $nuGetBuildNode = $projectXml.CreateElement('NuGetBuild', $projectXml.DocumentElement.NamespaceURI)
+    $nuGetBuildNode.InnerText = 'true'
+    $propertyGroup.AppendChild($nuGetBuildNode)
+
+    # Add the new PropertyGroup to the project as the first node, to make sure that it's fully applied.
+    $projectXml.Project.InsertBefore($propertyGroup, $projectXml.Project.FirstChild)
+
+    # Save the changes back to the .csproj file.
+    $projectXml.Save($projectFile)
+
+    # --no-restore because we run it in a separate step.
     dotnet add $projectFile.FullName package 'Microsoft.SourceLink.GitHub' --source 'https://api.nuget.org/v3/index.json' --no-restore
 }
 
