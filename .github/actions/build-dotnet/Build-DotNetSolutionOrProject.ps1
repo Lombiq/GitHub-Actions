32ced0ce20c9c5a633c2c6dcc259276b7a39bab8
Benedek Farkas <benedek.farkas@lombiq.com>
2024-06-14 17:35:30 +0200

    Updating some scripts to have proper function bodies

--- .github/actions/build-dotnet/Build-DotNetSolutionOrProject.ps1
+++ .github/actions/build-dotnet/Build-DotNetSolutionOrProject.ps1
@@ -1,4 +1,9 @@
-param (
+function ConvertTo-Array([string] $RawInput)
+{
+    $RawInput.Replace("`r", '').Split("`n") | ForEach-Object { $PSItem.Trim() } | Where-Object { $PSItem }
+}
+
+function Build-DotNetSolutionOrProject(
     [string] $Configuration,
     [string] $SolutionOrProject,
     [string] $Verbosity,
@@ -8,20 +13,15 @@ param (
     [string] $ExpectedCodeAnalysisErrors,
     [boolean] $CreateBinaryLog,
     [boolean] $WarningsAsErrors)
-
-function ConvertTo-Array([string] $rawInput)
 {
-    $rawInput.Replace("`r", '').Split("`n") | ForEach-Object { $PSItem.Trim() } | Where-Object { $PSItem }
-}
+    Write-Output "Version number for the .NET build products: $Version"
 
-Write-Output "Version number for the .NET build products: $Version"
+    # Notes on build switches that aren't self-explanatory:
+    # - -p:Retries and -p:RetryDelayMilliseconds are used to retry builds when they fail due to random locks.
+    # - --warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
+    #   errors." from breaking the build (since we treat warnings as errors).
 
-# Notes on build switches that aren't self-explanatory:
-# - -p:Retries and -p:RetryDelayMilliseconds are used to retry builds when they fail due to random locks.
-# - --warnAsMessage:MSB3026 is also to prevent random locks along the lines of "warning MSB3026: Could not copy dlls
-#   errors." from breaking the build (since we treat warnings as errors).
-
-$buildSwitches = ConvertTo-Array @"
+    $buildSwitches = ConvertTo-Array @"
     --configuration:$Configuration
     --nologo
     --verbosity:$Verbosity
@@ -37,88 +37,89 @@ $buildSwitches = ConvertTo-Array @"
     $Switches
 "@
 
-[array] $expectedErrorCodes = ConvertTo-Array $ExpectedCodeAnalysisErrors | ForEach-Object { $PSItem.Split(':')[0] } | Sort-Object
-$noErrors = $expectedErrorCodes.Count -eq 0
+    [array] $expectedErrorCodes = ConvertTo-Array $ExpectedCodeAnalysisErrors | ForEach-Object { $PSItem.Split(':')[0] } | Sort-Object
+    $noErrors = $expectedErrorCodes.Count -eq 0
 
-if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj)
-{
-    Write-Output '::group::Gulp Extensions found. It needs to be explicitly built before the solution.'
+    if (Test-Path src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj)
+    {
+        Write-Output '::group::Gulp Extensions found. It needs to be explicitly built before the solution.'
 
-    $startTime = [DateTime]::Now
-    dotnet build src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj @buildSwitches
-    $endTime = [DateTime]::Now
+        $startTime = [DateTime]::Now
+        dotnet build src/Utilities/Lombiq.Gulp.Extensions/Lombiq.Gulp.Extensions.csproj @buildSwitches
+        $endTime = [DateTime]::Now
 
-    Write-Output ('Gulp Extensions build took {0:0.###} seconds.' -f ($endTime - $startTime).TotalSeconds)
-    Write-Output '::endgroup::'
-}
+        Write-Output ('Gulp Extensions build took {0:0.###} seconds.' -f ($endTime - $startTime).TotalSeconds)
+        Write-Output '::endgroup::'
+    }
 
-# This prepares the solution or project with the Lombiq.Analyzers files. The output and exit code are discarded because
-# they will be in error if there is a project without the LombiqNetAnalyzers target. Then there is nothing to do, and
-# the target will still run on the projects that have it.
-dotnet msbuild '-target:Restore;LombiqNetAnalyzers' $SolutionOrProject | Out-Null || bash -c 'true'
+    # This prepares the solution or project with the Lombiq.Analyzers files. The output and exit code are discarded because
+    # they will be in error if there is a project without the LombiqNetAnalyzers target. Then there is nothing to do, and
+    # the target will still run on the projects that have it.
+    dotnet msbuild '-target:Restore;LombiqNetAnalyzers' $SolutionOrProject | Out-Null || bash -c 'true'
 
-Write-Output "Building solution or project with ``dotnet build $SolutionOrProject $($buildSwitches -join ' ')``."
+    Write-Output "Building solution or project with ``dotnet build $SolutionOrProject $($buildSwitches -join ' ')``."
 
-$errorLines = New-Object 'System.Collections.Generic.List[string]'
-$errorCodes = New-Object 'System.Collections.Generic.List[string]'
+    $errorLines = New-Object 'System.Collections.Generic.List[string]'
+    $errorCodes = New-Object 'System.Collections.Generic.List[string]'
 
-$errorFormat = '^(.*)\((\d+),(\d+)\): error (.*)'
-dotnet build $SolutionOrProject @buildSwitches 2>&1 | ForEach-Object {
-    if ($PSItem -notmatch $errorFormat) { return $PSItem }
+    $errorFormat = '^(.*)\((\d+),(\d+)\): error (.*)'
+    dotnet build $SolutionOrProject @buildSwitches 2>&1 | ForEach-Object {
+        if ($PSItem -notmatch $errorFormat) { return $PSItem }
 
     ($null, $file, $line, $column, $message) = [regex]::Match($PSItem, $errorFormat, 'Compiled').Groups.Value
 
-    $errorLines.Add($PSItem)
-    if ($message.Contains(':')) { $errorCodes.Add($message.Split(':')[0].Trim()) }
-    if ($noErrors) { Write-Output "::error file=$file,line=$line,col=$column::$message" }
-}
+        $errorLines.Add($PSItem)
+        if ($message.Contains(':')) { $errorCodes.Add($message.Split(':')[0].Trim()) }
+        if ($noErrors) { Write-Output "::error file=$file,line=$line,col=$column::$message" }
+    }
 
-if ($noErrors -and -not $?)
-{
-    exit 1
-}
-
-Stop-DotNetBuildServers
-
-if ($expectedErrorCodes)
-{
-    $errorCodes = $errorCodes | Sort-Object
-    $fail = 0
-    $report = New-Object 'System.Text.StringBuilder' "`n"
-
-    if ($null -eq $errorCodes -or -not $errorCodes.Count)
+    if ($noErrors -and -not $?)
     {
-        $expectedCount = $expectedErrorCodes.Count
-        $expectedCodesJoined = $expectedErrorCodes -join ', '
-
-        Write-Output "::error::Expected $expectedCount error codes ($expectedCodesJoined), but none were displayed."
         exit 1
     }
 
-    $length = [System.Math]::Max($errorCodes.Count, $expectedErrorCodes.Count)
-    foreach ($index in 0..($length - 1))
+    Stop-DotNetBuildServers
+
+    if ($expectedErrorCodes)
     {
-        $actual = $errorCodes[$index]
-        $expected = $expectedErrorCodes[$index]
+        $errorCodes = $errorCodes | Sort-Object
+        $fail = 0
+        $report = New-Object 'System.Text.StringBuilder' "`n"
 
-        if ($actual -eq $expected)
+        if ($null -eq $errorCodes -or -not $errorCodes.Count)
         {
-            $report.AppendLine("#$index OK ($actual)") | Out-Null
+            $expectedCount = $expectedErrorCodes.Count
+            $expectedCodesJoined = $expectedErrorCodes -join ', '
+
+            Write-Output "::error::Expected $expectedCount error codes ($expectedCodesJoined), but none were displayed."
+            exit 1
         }
-        else
+
+        $length = [System.Math]::Max($errorCodes.Count, $expectedErrorCodes.Count)
+        foreach ($index in 0..($length - 1))
         {
-            $report.AppendLine("#$index FAIL (expected: $expected; actual: $actual)") | Out-Null
-            $fail++
+            $actual = $errorCodes[$index]
+            $expected = $expectedErrorCodes[$index]
+
+            if ($actual -eq $expected)
+            {
+                $report.AppendLine("#$index OK ($actual)") | Out-Null
+            }
+            else
+            {
+                $report.AppendLine("#$index FAIL (expected: $expected; actual: $actual)") | Out-Null
+                $fail++
+            }
         }
-    }
 
-    if ($fail -gt 0)
-    {
-        Write-Warning $report.ToString() # We use warning so it doesn't stop prematurely.
-        Write-Output ('::error::Verification Mismatch ' + ($errorLines -join ' '))
-        exit 1
-    }
+        if ($fail -gt 0)
+        {
+            Write-Warning $report.ToString() # We use warning so it doesn't stop prematurely.
+            Write-Output ('::error::Verification Mismatch ' + ($errorLines -join ' '))
+            exit 1
+        }
 
-    Write-Output 'Verification complete, the solution or project only has the expected errors!'
-    exit 0
+        Write-Output 'Verification complete, the solution or project only has the expected errors!'
+        exit 0
+    }
 }
--- .github/actions/build-dotnet/Write-CacheConfiguration.ps1
+++ .github/actions/build-dotnet/Write-CacheConfiguration.ps1
@@ -1,25 +1,26 @@
-﻿param($IsNuget, $IsNpm, $RestoreKeys, $Hash)
+﻿function Invoke-Maybe($Block) { try { Invoke-Command -ScriptBlock $Block } catch { return } }
 
-function Invoke-Maybe($Block) { try { Invoke-Command -ScriptBlock $Block } catch { return } }
-
-$paths = @()
-
-if ($IsNuget) { $paths += , '~/.nuget/packages' }
-
-if ($IsNpm)
+function Write-CacheConfiguration($IsNuget, $IsNpm, $RestoreKeys, $Hash)
 {
+    $paths = @()
+
+    if ($IsNuget) { $paths += , '~/.nuget/packages' }
+
+    if ($IsNpm)
+    {
     (Invoke-Maybe { pnpm store path }), (npm config get cache) |
-        Where-Object { -not [string]::IsNullOrEmpty($PSItem) } |
-        ForEach-Object { $paths += $PSItem }
+            Where-Object { -not [string]::IsNullOrEmpty($PSItem) } |
+            ForEach-Object { $paths += $PSItem }
+    }
+
+    # Ensure the paths exist.
+    $paths | ForEach-Object { New-Item -ItemType Directory -Force $PSItem } | Out-Null
+
+    # Multiple paths must be separated by "\n", but we can't include newline in the workflow command so we have to misuse
+    # the format function like this.
+    Set-GitHubOutput 'paths' ($paths -join '{0}')
+
+    Set-GitHubOutput 'cache-enabled' 'true'
+    Set-GitHubOutput 'key' "${RestoreKeys}-${Hash}"
+    Set-GitHubOutput 'restore-keys' $RestoreKeys
 }
-
-# Ensure the paths exist.
-$paths | ForEach-Object { New-Item -ItemType Directory -Force $PSItem } | Out-Null
-
-# Multiple paths must be separated by "\n", but we can't include newline in the workflow command so we have to misuse
-# the format function like this.
-Set-GitHubOutput 'paths' ($paths -join '{0}')
-
-Set-GitHubOutput 'cache-enabled' 'true'
-Set-GitHubOutput 'key' "${RestoreKeys}-${Hash}"
-Set-GitHubOutput 'restore-keys' $RestoreKeys
