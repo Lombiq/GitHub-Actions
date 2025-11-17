c233e58c97a220a0307bb12724c2ba25acd1faff
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-11-17 17:51:48 +0100

    Fixing formatting

--- .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
+++ .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
@@ -1,7 +1,7 @@
 $initialSpace = (df / |
-    Select-Object -Skip 1 |
-    ForEach-Object { $PSItem.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[3] } |
-    Measure-Object -Sum).Sum
+        Select-Object -Skip 1 |
+        ForEach-Object { $PSItem.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[3] } |
+        Measure-Object -Sum).Sum
 
 df --human-readable
 
@@ -33,8 +33,8 @@ sudo rm -rf /usr/local/julia*
 df --human-readable
 
 $finalSpace = (df / |
-    Select-Object -Skip 1 |
-    ForEach-Object { $PSItem.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[3] } |
-    Measure-Object -Sum).Sum
+        Select-Object -Skip 1 |
+        ForEach-Object { $PSItem.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[3] } |
+        Measure-Object -Sum).Sum
 $freedSpace = [math]::Round(($finalSpace - $initialSpace) / 1024 / 1024, 2)
 Write-Output "Freed up approximately $freedSpace GB of storage space."
