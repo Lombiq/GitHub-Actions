a95c6127398d26b12bcaa087a04451757eb3d095
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-11-19 17:00:58 +0100

    Back to sudo rm -rf

--- .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
+++ .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
@@ -9,28 +9,28 @@ df --human-readable
 sudo apt-get clean
 
 # Remove Android SDK.
-Remove-Item -Recurse -Force /usr/local/lib/android
+sudo rm -rf /usr/local/lib/android
 
 # We can't remove all unused Docker images since that would also remove the Elasticsearch image (used in the Set up
 # Elasticsearch step) and any possible Docker images from prepared runner images.
-# docker image prune --all --force
+# sudo docker image prune --all --force
 
 # We can't remove the unused build cache since that would also remove the Elasticsearch build cache (used in the Set up
 # Elasticsearch step) and any possible caches from prepared runner images.
-# docker builder prune --all --force
+# sudo docker builder prune --all --force
 
 # Remove Java (JDKs).
-Remove-Item -Recurse -Force /usr/lib/jvm
+sudo rm -rf /usr/lib/jvm
 
 # Remove Swift toolchain.
-Remove-Item -Recurse -Force /usr/share/swift
+sudo rm -rf /usr/share/swift
 
 # Remove Haskell (GHC).
-Remove-Item -Recurse -Force /opt/ghc
-Remove-Item -Recurse -Force /usr/local/.ghcup
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/.ghcup
 
 # Remove Julia.
-Remove-Item -Recurse -Force /usr/local/julia*
+sudo rm -rf /usr/local/julia*
 
 df --human-readable
 
