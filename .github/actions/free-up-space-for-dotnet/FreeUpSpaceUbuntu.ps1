d4f413f4b73488ba1f90149d547cbd95a3cad188
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-11-19 16:48:38 +0100

    Do we actually need sudo?

--- .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
+++ .github/actions/free-up-space-for-dotnet/FreeUpSpaceUbuntu.ps1
@@ -6,31 +6,31 @@ $initialSpace = (df / |
 df --human-readable
 
 # Remove the local cache of downloaded packages but don't remove installed software.
-sudo apt-get clean
+apt-get clean
 
 # Remove Android SDK.
-sudo Remove-Item -Recurse -Force /usr/local/lib/android
+Remove-Item -Recurse -Force /usr/local/lib/android
 
 # We can't remove all unused Docker images since that would also remove the Elasticsearch image (used in the Set up
 # Elasticsearch step) and any possible Docker images from prepared runner images.
-# sudo docker image prune --all --force
+# docker image prune --all --force
 
 # We can't remove the unused build cache since that would also remove the Elasticsearch build cache (used in the Set up
 # Elasticsearch step) and any possible caches from prepared runner images.
-# sudo docker builder prune --all --force
+# docker builder prune --all --force
 
 # Remove Java (JDKs).
-sudo Remove-Item -Recurse -Force /usr/lib/jvm
+Remove-Item -Recurse -Force /usr/lib/jvm
 
 # Remove Swift toolchain.
-sudo Remove-Item -Recurse -Force /usr/share/swift
+Remove-Item -Recurse -Force /usr/share/swift
 
 # Remove Haskell (GHC).
-sudo Remove-Item -Recurse -Force /opt/ghc
-sudo Remove-Item -Recurse -Force /usr/local/.ghcup
+Remove-Item -Recurse -Force /opt/ghc
+Remove-Item -Recurse -Force /usr/local/.ghcup
 
 # Remove Julia.
-sudo Remove-Item -Recurse -Force /usr/local/julia*
+Remove-Item -Recurse -Force /usr/local/julia*
 
 df --human-readable
 
