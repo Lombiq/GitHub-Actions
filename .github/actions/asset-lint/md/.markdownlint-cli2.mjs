3fc0e9ddfdeaa7f9daaa84450ca2d24fe8242b0f
Sára El-Saig <sara.el-saig@lombiq.com>
2025-11-22 14:45:07 +0100

    Fix ESLint warnings.

--- .github/actions/asset-lint/md/.markdownlint-cli2.mjs
+++ .github/actions/asset-lint/md/.markdownlint-cli2.mjs
@@ -1,16 +1,20 @@
 import fs from 'node:fs';
 
 export default {
-    "ignores": fs.readFileSync('.markdownlintignore', 'utf8').split('\n').map((line) => line.split('#')[0].trim()).filter((line) => line.length),
-    "config":
+    ignores: fs
+        .readFileSync('.markdownlintignore', 'utf8')
+        .split('\n')
+        .map((line) => line.split('#')[0].trim())
+        .filter((line) => line.length),
+    config:
     {
-        "default": true,
-        "MD004": { "style": "dash" },
-        "MD013": false,
-        "MD028": false,
-        "MD033": { "allowed_elements": [ "kbd" ] },
-        "MD049": { "style": "underscore" },
-        "MD050": { "style": "asterisk" },
-        "MD059": false,
-    }
+        default: true,
+        MD004: { style: 'dash' },
+        MD013: false,
+        MD028: false,
+        MD033: { allowed_elements: [ 'kbd' ] },
+        MD049: { style: 'underscore' },
+        MD050: { style: 'asterisk' },
+        MD059: false,
+    },
 };
--- .github/actions/asset-lint/md/.textlintrc.js
+++ .github/actions/asset-lint/md/.textlintrc.js
@@ -1,18 +1,18 @@
 const textLintConfig = {
-  plugins: {},
-  filters: {
-    comments: true
-  },
-  rules: {
-    'common-misspellings': true,
-    'max-comma': true,
-    'no-empty-section': true,
-    'no-todo': true,
-    'no-zero-width-spaces': true,
-    'no-start-duplicated-conjunction': true,
-    // This rule generates a lot of false positives on Windows. False negatives are avoided by linting on Linux too.
-    'doubled-spaces': process.platform !== 'win32',
-  }
+    plugins: {},
+    filters: {
+        comments: true,
+    },
+    rules: {
+        'common-misspellings': true,
+        'max-comma': true,
+        'no-empty-section': true,
+        'no-todo': true,
+        'no-zero-width-spaces': true,
+        'no-start-duplicated-conjunction': true,
+        // This rule generates a lot of false positives on Windows. False negatives are avoided by linting on Linux too.
+        'doubled-spaces': process.platform !== 'win32',
+    },
 };
 
 module.exports = textLintConfig;
