6419df2f625b2652b19a75bd8d2fe721e2d3227e
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-22 01:43:32 +0100

    Extract Elasticsearch startup logic into a standalone script.

---
+++ .github/actions/setup-elasticsearch/Start-Elasticsearch.ps1
@@ -0,0 +1,35 @@
+# Install Docker containers using "docker compose".
+New-Item -ItemType Directory -Path Elasticsearch
+Set-Location Elasticsearch
+Copy-Item (Join-Path $PSScriptRoot docker-compose.yml) .
+docker compose up --wait
+docker compose start
+
+# Test server status.
+function Test-Elasticsearch()
+{
+    try
+    {
+        $result = Invoke-WebRequest http://localhost:9200/
+        Write-Output $result
+        return $True
+    }
+    catch
+    {
+        return $False
+    }
+}
+
+for ($i = 0; $i -lt 10; $i += 1)
+{
+    if (Test-Elasticsearch)
+    {
+        Write-Output ok
+        break
+    }
+    else
+    {
+        Write-Output 'Wait a few seconds!'
+        Start-Sleep -Seconds 3
+    }
+}
