97a769a18b3d619ff83a081f5c35195e805fe89a
Zoltan Lehoczky <zoltan.lehoczky@lombiq.com>
2026-09-23 22:59:12 +0200

    Disabling MD line length checking

--- .github/actions/asset-lint/md/.textlintrc.js
+++ .github/actions/asset-lint/md/.textlintrc.js
@@ -5,6 +5,8 @@ const textLintConfig = {
     },
     rules: {
         'common-misspellings': true,
+        // Text files are usually edited with word wrap, no need to enforce line length.
+        'line-length': false,
         'max-comma': true,
         'no-empty-section': true,
         'no-todo': true,
